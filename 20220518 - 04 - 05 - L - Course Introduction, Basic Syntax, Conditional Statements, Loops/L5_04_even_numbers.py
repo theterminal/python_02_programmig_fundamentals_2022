b60ce0694605f83622cfd1a18cf0c19fa7009512
theterminal <kirilk777@gmail.com>
2022-06-06 23:09:34 +0300

    Initial commit

---
+++ 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/L5_04_even_numbers.py
@@ -0,0 +1,12 @@
+# 20220518 - Python - Python Fundamentals - L5 - Basic Syntax
+# 04 - Even Numbers - judge url: https://judge.softuni.org/Contests/Practice/Index/1718#3
+
+n = int(input())
+
+for i in range(n):
+    num = int(input())
+    if num % 2 != 0:
+        print(f'{num} is odd!')
+        break
+else:
+    print('All numbers are even.')
