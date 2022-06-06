f2c36506e755bfe9027da8a5559ef52444e84225
theterminal <kirilk777@gmail.com>
2022-06-06 22:41:36 +0300

    Initial commit

---
+++ 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/L5_01_number_definer.py
@@ -0,0 +1,21 @@
+# 20220518 - Python - Python Fundamentals - L5 - Basic Syntax
+# 01 - Number Definer - judge url: https://judge.softuni.org/Contests/Practice/Index/1718#0
+
+num_entered = float(input())
+
+if num_entered == 0:
+    print('zero')
+elif num_entered > 0:
+    if num_entered < 1:
+        print('small positive')
+    elif num_entered > 100000:
+        print('large positive')
+    else:
+        print('positive')
+else:
+    if num_entered > -1:
+        print('small negative')
+    elif num_entered < -1000000:
+        print('large negative')
+    else:
+        print('negative')
