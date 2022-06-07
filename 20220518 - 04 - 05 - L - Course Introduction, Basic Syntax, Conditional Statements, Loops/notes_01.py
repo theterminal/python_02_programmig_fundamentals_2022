b1e44596c25d8aba7dfd5f95f854b03d738b729c
theterminal <kirilk777@gmail.com>
2022-06-07 11:18:41 +0300

    Initial commit

---
+++ 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/notes_01.py
@@ -0,0 +1,26 @@
+# 20220518 - Python - Python Fundamentals - L5 - Basic Syntax
+# Notes
+# This file is not intended for execution!
+
+# Storing range into variable
+a = range(1, 10)
+for i in a:
+    print(i, end=", ")
+
+# -----------------------------------------------------------------
+
+# this example shows that 'not' has the highest priority
+if 2 > 1 or 3 < 4 and not 4 > 0:
+    print('This will be printed')
+
+# this example shows that 'and' is second in priority after 'not'
+if (2 > 1 or 3 < 4) and not 4 > 0:
+    print('This will NOT be printed')
+
+# -----------------------------------------------------------------
+
+# when iterable variable is not needed can use '_' only
+for _ in range(45):
+    print(65)
+
+# -----------------------------------------------------------------
