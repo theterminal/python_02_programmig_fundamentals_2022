ae855cb5b168611a1f8ba4e7bfe4c71dbaa28b96
theterminal <kirilk777@gmail.com>
2022-06-07 11:16:11 +0300

    Initial commit

---
+++ 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/L5_07_patterns_ver02.py
@@ -0,0 +1,16 @@
+# 20220518 - Python - Python Fundamentals - L5 - Basic Syntax
+# 07 - Patterns - judge url: https://judge.softuni.org/Contests/Practice/Index/1718#6
+
+# ------------- version 2 -----------------------
+
+num_stars_max_per_row = int(input())
+
+for num_stars_to_print in range(1, num_stars_max_per_row + 1):
+    for j in range(0, num_stars_to_print):
+        print('*', end="")
+    print()
+
+for num_stars_to_print in range(num_stars_max_per_row - 1, 0, -1):
+    for j in range(0, num_stars_to_print):
+        print('*', end="")
+    print()
