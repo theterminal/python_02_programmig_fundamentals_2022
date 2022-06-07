999ae55422d3413202bd53dd8a4ea8df94f1c298
theterminal <kirilk777@gmail.com>
2022-06-07 11:24:17 +0300

    Changed file name and split different versions into different files.

---
+++ 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/L5_02_largest_of_three_numbers_ver02.py
@@ -0,0 +1,13 @@
+# 20220518 - Python - Python Fundamentals - L5 - Basic Syntax
+# 02 - Largest of Three Numbers - judge url: https://judge.softuni.org/Contests/Practice/Index/1718#1
+
+# ------------------ version 2 ------------------------
+
+num_1, num_2, num_3 = int(input()), int(input()), int(input())
+
+if num_1 > num_2 and num_1 > num_3:
+    print(num_1)
+elif num_2 > num_1 and num_2 > num_3:
+    print(num_2)
+else:
+    print(num_3)
