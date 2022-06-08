5185d3b46c330f9b7b2276b68973eeca539a05d6
theterminal <kirilk777@gmail.com>
2022-06-08 07:24:08 +0300

    Added a note as description.

--- 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/L5_07_patterns_ver01.py
+++ 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/L5_07_patterns_ver01.py
@@ -1,4 +1,4 @@
-# 20220518 - Python - Python Fundamentals - L5 - Basic Syntax
+# 20220518 - Python - Python Fundamentals - L5 - Basic Syntax, Conditional Statements, Loops
 # 07 - Patterns - judge url: https://judge.softuni.org/Contests/Practice/Index/1718#6
 
 # ------------ version 1 -----------------------
