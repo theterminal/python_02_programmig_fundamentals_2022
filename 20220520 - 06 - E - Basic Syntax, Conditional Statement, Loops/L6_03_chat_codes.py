268993529e0a0187dacc009cb44b51312abcca1e
theterminal <kirilk777@gmail.com>
2022-06-08 07:33:29 +0300

    Initial commit.

---
+++ 20220520 - 06 - E - Basic Syntax, Conditional Statement, Loops/L6_03_chat_codes.py
@@ -0,0 +1,16 @@
+# 20220521 - Python - Python Fundamentals - L6 - Basic Syntax, Conditional Statements, Loops
+# 03 - Chat Codes - judge url: https://judge.softuni.org/Contests/Compete/Index/1719#2
+
+num_messages = int(input())
+
+for _ in range(num_messages):
+    code = int(input())
+
+    if code == 88:
+        print('Hello')
+    elif code == 86:
+        print('How are you?')
+    elif code < 88 and code != 86:
+        print('GREAT!')
+    elif code > 88:
+        print('Bye.')
