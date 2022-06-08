f36ab91eaee3efd2e18da5d8317c36a2dee0cd12
theterminal <kirilk777@gmail.com>
2022-06-08 07:36:18 +0300

    Initial commit.

---
+++ 20220520 - 06 - E - Basic Syntax, Conditional Statement, Loops/L6_04_maximum_multiple.py
@@ -0,0 +1,20 @@
+# 20220521 - Python - Python Fundamentals - L6 - Basic Syntax, Conditional Statements, Loops
+# 04 - Maximum Multiple - judge url: https://judge.softuni.org/Contests/Compete/Index/1719#3
+
+divisor = int(input())
+boundary = int(input())
+max_multiplier = 0
+
+for current_num in range(boundary, divisor, -1):
+    if current_num % divisor == 0:
+        max_multiplier = current_num
+        print(max_multiplier)
+        break
+
+# ---------------------------------------------------
+# # interesting concept from Юри Димитров
+
+# divisor = int(input())
+# boundary = int(input())
+
+# print(boundary // divisor * divisor)
