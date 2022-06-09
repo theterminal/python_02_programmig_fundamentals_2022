f5f65eb955230d4e4b1b3f1a075f675ba3da3917
theterminal <kirilk777@gmail.com>
2022-06-09 07:31:09 +0300

    Initial commit.

---
+++ 20220520 - 06 - E - Basic Syntax, Conditional Statement, Loops/L6_more_03_wolf_in_sheep's_clothing.py
@@ -0,0 +1,16 @@
+# 20220528 - Python - Python Fundamentals - L6 - Basic Syntax, Conditional Statements, Loops
+# More Exercise 03 - Wolf in Sheep's Clothing - judge url: https://judge.softuni.org/Contests/Practice/Index/1720#2
+
+str_input = input()
+
+lst_1 = str_input.split(', ')               # turns string into a list by splitting it by comma and space
+index_wolf = ''
+
+for i in range(len(lst_1)):
+    if lst_1[i] == 'wolf':
+        index_wolf = i
+
+if index_wolf == (len(lst_1) - 1):
+    print('Please go away and stop eating my sheep')
+else:
+    print(f'Oi! Sheep number {len(lst_1) - index_wolf - 1}! You are about to be eaten by a wolf!')
