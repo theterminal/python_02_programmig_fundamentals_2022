e62f7e21d8c904663a2e8e9454bdc295b7447eed
theterminal <kirilk777@gmail.com>
2022-06-10 07:09:15 +0300

    Initial commit.

---
+++ 20220525 - 08 - L - Data Types and Variables/L8_notes_01_locations_and_addresses.py
@@ -0,0 +1,12 @@
+# 20220529 - Python - Python Fundamentals - L8 - Data Types and Variables
+# notes 01 - Locations and Addresses
+
+a = None
+b = None
+c = None
+
+print(id(a))            # All ids are pointing to the same location for None
+print(id(b))            # All ids are pointing to the same location for None
+print(id(c))            # All ids are pointing to the same location for None
+
+print(type(None))       # see the result
