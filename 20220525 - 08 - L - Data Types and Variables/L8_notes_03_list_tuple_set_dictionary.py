81482c1657604f964f8ed1449753b6cf5f098f9c
theterminal <kirilk777@gmail.com>
2022-06-10 07:10:50 +0300

    Initial commit.

---
+++ 20220525 - 08 - L - Data Types and Variables/L8_notes_03_list_tuple_set_dictionary.py
@@ -0,0 +1,24 @@
+# 20220529 - Python - Python Fundamentals - L8 - Data Types and Variables
+# notes 03 - List, Tuple, Set, Dictionary
+
+print('\n------- Example 1 ---------------------------- printing the content of a dictionary ------------------\n')
+dict_1 = {'name': 'Ivan', 'age': 42, 'smarts': 'very'}
+print(dict_1)
+
+print('\n------- Example 2 ---------------------------- printing a \'value\' of a \'key\' from a dictionary -------\n')
+print(dict_1['name'])
+
+print('\n------- Example 3 ---------------------------- printing the type of a dictionary ---------------------\n')
+print(type(dict_1))
+
+print('\n------- Example 4 ---------------------------- printing the items of a dictionary --------------------\n')
+print(dict_1.items())
+print(dict_1.items)
+
+print('\n------- Example 5 ---------------------------- printing the keys of a dictionary ---------------------\n')
+print(dict_1.keys())
+print(dict_1.keys)
+
+print('\n------- Example 6 ---------------------------- printing the values of a dictionary -------------------\n')
+print(dict_1.values())
+print(dict_1.values)
