097a9edbc44658df8c5e972218b27530947b4cf5
theterminal <kirilk777@gmail.com>
2022-06-10 07:07:59 +0300

    Initial commit.

---
+++ 20220525 - 08 - L - Data Types and Variables/L8_01_concat_names.py
@@ -0,0 +1,8 @@
+# 20220529 - Python - Python Fundamentals - L9 - Data Types and Variables
+# 01 - Concat Names - judge url: https://judge.softuni.org/Contests/Practice/Index/1721#0
+
+name_1 = input()
+name_2 = input()
+delimiter = input()
+
+print(f'{name_1}{delimiter}{name_2}')
