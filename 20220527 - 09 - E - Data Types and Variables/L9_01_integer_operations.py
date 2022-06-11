f8e045ed4e2c24d945d0fcdbae57b59b723070db
theterminal <kirilk777@gmail.com>
2022-06-11 22:25:43 +0300

    Initial commit.

---
+++ 20220527 - 09 - E - Data Types and Variables/L9_01_integer_operations.py
@@ -0,0 +1,6 @@
+# 20220527 - Python - Python Fundamentals - L9 - Data Types and Variables
+# 01 - integer Operations - judge url: https://judge.softuni.org/Contests/Compete/Index/1722#0
+
+num_1, num_2, num_3, num_4 = int(input()), int(input()), int(input()), int(input())
+
+print(int((int((num_1 + num_2) / num_3)) * num_4))
