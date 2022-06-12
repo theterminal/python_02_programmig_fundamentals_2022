81a0a8019bbf33bc6698564682a0b94d12ccedfb
theterminal <kirilk777@gmail.com>
2022-06-12 08:29:58 +0300

    Initial commit.

---
+++ 20220527 - 09 - E - Data Types and Variables/L9_more_03_decrypting_messages.py
@@ -0,0 +1,25 @@
+# 20220528 - Python - Python Fundamentals - L9 - Data Types and Variables
+# More Exercises 03 - Decrypting Messages - judge url: https://judge.softuni.org/Contests/Practice/Index/1723#2
+
+# ------- version 2 ----------
+
+key = int(input())
+num_lines = range(int(input()))
+str_to_print = ''
+
+for _ in num_lines:
+    str_to_print += chr(ord(input()) + key)
+
+print(str_to_print)
+
+# ------- version 1 -----------
+
+key = int(input())
+num_lines = range(int(input()))
+str_to_print = ''
+
+for _ in num_lines:
+    char = ord(input())
+    str_to_print += chr(char + key)
+
+print(str_to_print)
