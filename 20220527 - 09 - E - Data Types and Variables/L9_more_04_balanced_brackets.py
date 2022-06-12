9ecfc56a2455fa774afae4b042f3fd6369db71be
theterminal <kirilk777@gmail.com>
2022-06-12 08:30:08 +0300

    Initial commit.

---
+++ 20220527 - 09 - E - Data Types and Variables/L9_more_04_balanced_brackets.py
@@ -0,0 +1,27 @@
+# 20220528 - Python - Python Fundamentals - L9 - Data Types and Variables
+# More Exercises 04 - Balanced Brackets - judge url: https://judge.softuni.org/Contests/Practice/Index/1723#3
+
+num_lines = int(input())
+counter, flag = 0, False
+
+for _ in range(num_lines):
+    char = input()
+
+    if char == '(':
+        counter += 1
+
+        if counter > 1:
+            flag = True
+            break
+
+    if char == ')':
+        if counter != 1:
+            flag = True
+            break
+        else:
+            counter = 0
+
+if flag:
+    print('UNBALANCED')
+else:
+    print('BALANCED')
