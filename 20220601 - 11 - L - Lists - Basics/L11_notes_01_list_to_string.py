2288b24128c625f7eb432f8bfb80be5292cefb66
theterminal <kirilk777@gmail.com>
2022-06-12 08:34:55 +0300

    Initial commit.

---
+++ 20220601 - 11 - L - Lists - Basics/L11_notes_01_list_to_string.py
@@ -0,0 +1,14 @@
+# 20220601 - Python Code - L11 - Lists Basics
+# String to List conversion
+
+# list example
+lst = ['Garry', 'Eric', 'Bill']
+
+# creating a string 'str_from_list' from the list 'lst' with separators ', '
+str_from_list = (', '.join(lst))
+
+# printing the string 'str_from_lst'
+print(str_from_list)
+
+# printing the list 'lst' to show that it still exists and it is not converted
+print(lst)
