14f6f5c90e4879f1bef711faebb98692e59a791e
theterminal <kirilk777@gmail.com>
2022-06-13 11:04:59 +0300

    Initial commit.

---
+++ 20220603 - 12 - E - Lists - Basics/L12_01_invert_values.py
@@ -0,0 +1,10 @@
+# 20220603 - Python Code - L12 - Lists Basics
+# 01 - Invert Values - judge url: https://judge.softuni.org/Contests/Compete/Index/1725#0
+
+list_start_values = input().split(' ')
+list_opposite_values = list()
+
+for element in list_start_values:
+    list_opposite_values.append(-int(element))
+
+print(list_opposite_values)
