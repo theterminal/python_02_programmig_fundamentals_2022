1fe393d5317b7ed033c0c6e13dda7ed77c9b9748
theterminal <kirilk777@gmail.com>
2022-06-14 06:50:10 +0300

    Added a comment.

--- 20220603 - 12 - E - Lists - Basics/L12_01_invert_values.py
+++ 20220603 - 12 - E - Lists - Basics/L12_01_invert_values.py
@@ -1,4 +1,4 @@
-# 20220603 - Python Code - L12 - Lists Basics
+# 20220603 - Python Code - L12 - Lists Basics - Exercise
 # 01 - Invert Values - judge url: https://judge.softuni.org/Contests/Compete/Index/1725#0
 
 list_start_values = input().split(' ')
