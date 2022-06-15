305cbe5d3fd56ee1d06628e828352daa2c1159e9
theterminal <kirilk777@gmail.com>
2022-06-15 07:52:44 +0300

    Initial comment.

---
+++ 20220608 - 14 - L - Functions/L14_06_calculate_rectangle_area.py
@@ -0,0 +1,15 @@
+# 20220614 - Python Code - Functions - Lecture
+# 06 - Calculate Rectangle Area - judge url: https://judge.softuni.org/Contests/Practice/Index/1727#5
+
+
+def rectangle_area(width: int, height: int):
+
+    result = width * height
+
+    return result
+
+
+w = int(input())
+h = int(input())
+print(rectangle_area(w, h))
+
