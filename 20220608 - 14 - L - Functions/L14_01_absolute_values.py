fd678ac3d2c5bcbaf2a96b08501492f78616c9e0
theterminal <kirilk777@gmail.com>
2022-06-16 06:34:02 +0300

    Added comments

--- 20220608 - 14 - L - Functions/L14_01_absolute_values.py
+++ 20220608 - 14 - L - Functions/L14_01_absolute_values.py
@@ -1,10 +1,14 @@
 # 20220608 - Python Code - Functions - Lecture
 # 01 - Absolute Values - judge url: https://judge.softuni.org/Contests/Practice/Index/1727#0
 
-# str_entered = input().split()
-# lst_from_str_entered_to_abs_num = [abs(float(i)) for i in str_entered]
-#
-# print(lst_from_str_entered_to_abs_num)
+
+# ---------------- no function ---------------------
+
+
+str_entered = input().split()
+lst_from_str_entered_to_abs_num = [abs(float(i)) for i in str_entered]
+
+print(lst_from_str_entered_to_abs_num)
 
 
 # ---------------- with function ---------------------
