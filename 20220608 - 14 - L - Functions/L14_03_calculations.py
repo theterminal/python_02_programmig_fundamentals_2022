3870c969111691871a05fe0dd04a950ebaa6a239
theterminal <kirilk777@gmail.com>
2022-06-16 06:34:45 +0300

    Added comments

--- 20220608 - 14 - L - Functions/L14_03_calculations.py
+++ 20220608 - 14 - L - Functions/L14_03_calculations.py
@@ -1,32 +1,33 @@
 # 20220614 - Python Code - Functions - Lecture
 # 03 - Calculations - judge url: https://judge.softuni.org/Contests/Practice/Index/1727#2
 
+
 # ------- using normal function --------
 
 
-# def calculations(operator, int_1, int_2):
-#
-#     result = None
-#
-#     if operator == 'multiply':
-#         result = int_1 * int_2
-#
-#     elif operator == 'divide':
-#         result = int(int_1 / int_2)
-#
-#     elif operator == 'add':
-#         result = int_1 + int_2
-#
-#     elif operator == 'subtract':
-#         result = int_1 - int_2
-#
-#     return result
-#
-#
-# in_1 = input()
-# in_2 = int(input())
-# in_3 = int(input())
-# print(calculations(in_1, in_2, in_3))
+def calculations(operator_1, int_1, int_2):
+    result = None
+
+    if operator_1 == 'multiply':
+        result = int_1 * int_2
+
+    elif operator_1 == 'divide':
+        result = int(int_1 / int_2)
+
+    elif operator_1 == 'add':
+        result = int_1 + int_2
+
+    elif operator_1 == 'subtract':
+        result = int_1 - int_2
+
+    return result
+
+
+in_1 = input()
+in_2 = int(input())
+in_3 = int(input())
+
+print(calculations(in_1, in_2, in_3))
 
 
 # ------- using import operator module --------
@@ -45,4 +46,5 @@ def repeat_string_2(operation, num_1, num_2):
 in_1 = input()
 in_2 = int(input())
 in_3 = int(input())
+
 print(repeat_string_2(in_1, in_2, in_3))
