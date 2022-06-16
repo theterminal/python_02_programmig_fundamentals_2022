50ecaa9696fd990d99025166df7f452ad30e69f2
theterminal <kirilk777@gmail.com>
2022-06-16 06:36:50 +0300

    Initial commit.

---
+++ 20220608 - 14 - L - Functions/L14_notes_02_creating_invoking.py
@@ -0,0 +1,39 @@
+# 20220608 - Python Code - Functions - Lecture
+# 02 - Creating and Invoking a Functions
+
+# --------------------- example 1 ----------------
+
+
+def print_header():
+    print('\nPrinted from the function')
+
+
+print_header()
+
+
+# --------------------- example 2 ----------------
+
+
+def print_footer():
+    print('This is the footer of page!\n')
+
+
+def display_page():
+    print_header()
+    print_footer()
+
+
+display_page()
+
+
+# ---------------------- recursion ---------------
+
+
+def crash():
+    crash()
+
+
+# crash()
+
+
+# ------------------------------------------------
