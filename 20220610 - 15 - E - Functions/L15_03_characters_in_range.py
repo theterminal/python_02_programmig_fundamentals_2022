58cb8111eac9b3333effc8e85036627a40cb7d1f
theterminal <kirilk777@gmail.com>
2022-06-17 13:05:56 +0300

    Initial commit.

---
+++ 20220610 - 15 - E - Functions/L15_03_characters_in_range.py
@@ -0,0 +1,66 @@
+# 20220614 - Python Code - Functions - Exercise
+# 03 - Characters in Range - judge url: https://judge.softuni.org/Contests/Compete/Index/1728#2
+
+
+# ---------- version 3 ---------------------
+
+
+def char_in_range(z1, z2):
+    asc_start = ord(z1)
+    asc_end = ord(z2)
+
+    result = []
+
+    for i in range(asc_start + 1, asc_end):
+        result.append(chr(i))
+
+    return result
+
+
+char_1 = input()
+char_2 = input()
+
+output = (char_in_range(char_1, char_2))
+print(*output)                                  # only line different than the version 2
+
+# ---------- version 2 ---------------------
+
+
+def char_in_range(z1, z2):
+    asc_start = ord(z1)
+    asc_end = ord(z2)
+
+    result = []
+
+    for i in range(asc_start + 1, asc_end):
+        result.append(chr(i))
+
+    return result
+
+
+char_1 = input()
+char_2 = input()
+
+output = (char_in_range(char_1, char_2))
+print(' '.join(output))
+
+
+# ---------- version 1 ---------------------
+
+
+def char_in_range(z1, z2):
+    asc_start = ord(z1)
+    asc_end = ord(z2)
+
+    result = ''
+
+    for i in range(asc_start + 1, asc_end):
+        result += chr(i) + ' '
+
+    return result
+
+
+char_1 = input()
+char_2 = input()
+
+print(char_in_range(char_1, char_2))
