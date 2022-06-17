85af3ca2ba8bc12f4e3ba0fcc2eeda03b4f449ca
theterminal <kirilk777@gmail.com>
2022-06-17 13:08:24 +0300

    Initial commit.

---
+++ 20220610 - 15 - E - Functions/L15_06_sort.py
@@ -0,0 +1,6 @@
+# 20220614 - Python Code - Functions - Exercise
+# 06 - Sort - judge url: https://judge.softuni.org/Contests/Compete/Index/1728#5
+
+# ----------- version 1 ---------------------------
+
+print(sorted([int(i) for i in input().split(' ')]))
