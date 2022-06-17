e4d7172b7d9d4361e8b91c3b394ae147d26118da
theterminal <kirilk777@gmail.com>
2022-06-17 13:09:02 +0300

    Initial commit.

---
+++ 20220610 - 15 - E - Functions/L15_07_min_max_sum.py
@@ -0,0 +1,26 @@
+# 20220614 - Python Code - Functions - Exercise
+# 07 - Mim Max and Sum - judge url: https://judge.softuni.org/Contests/Compete/Index/1728#6
+
+
+# ------------------- version 2 -----------------------
+
+lst_nums = [int(i) for i in input().split()]
+print(f'The minimum number is {min(lst_nums)}')
+print(f'The maximum number is {max(lst_nums)}')
+print(f'The sum number is: {sum(lst_nums)}')
+
+
+# ------------------- version 1 -----------------------
+
+
+def min_max_sum(lst_ints: list):
+    min_num = min(lst_ints)
+    max_num = max(lst_ints)
+    sum_all_nums = sum(lst_ints)
+
+    return f'The minimum number is {min_num}\nThe maximum number is {max_num}\nThe sum number is: {sum_all_nums}'
+
+
+lst_nums = [int(i) for i in input().split()]
+result = min_max_sum(lst_nums)
+print(result)
