9cc9543ba09d455df9ea8c16bfc522d319af1eb3
theterminal <kirilk777@gmail.com>
2022-06-20 13:53:02 +0300

    Initial commit.

---
+++ 20220610 - 15 - E - Functions/L15_11_loading_bar.py
@@ -0,0 +1,15 @@
+# 20220615 - Python Code - Functions - Exercise
+# 11 - Loading Bar - judge url: https://judge.softuni.org/Contests/Compete/Index/1728#10
+
+def loading_bar(num):
+    if num == 100:
+        return f'{num}% Complete!\n[%%%%%%%%%%]'
+
+    bar_l = int(num / 10) * '%'
+    bar_r = (10 - int(num / 10)) * '.'
+    bar = bar_l + bar_r
+    return f'{num}% [{bar}]\nStill loading...'
+
+
+int_input_div_10 = int(input())
+print(loading_bar(int_input_div_10))
