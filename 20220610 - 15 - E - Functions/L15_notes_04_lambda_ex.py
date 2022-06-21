e7a51402393eee03550ec46fa9be465d4f51e5ca
theterminal <kirilk777@gmail.com>
2022-06-21 10:28:32 +0300

    Initial commit.

---
+++ 20220610 - 15 - E - Functions/L15_notes_04_lambda_ex.py
@@ -0,0 +1,15 @@
+# 20220616 - Python Code
+# Lambda examples
+
+# ----------- 01 ---------------
+
+test_1 = lambda x: x * 10
+
+d = test_1(5)
+print(d)
+
+# ----------- 02 ---------------
+
+test_2 = lambda x: (x * x) + (3 * x) - 5
+
+print(test_2(1))
