a7c6718ee8b832684adc6eea09b33e01bdc4d230
theterminal <kirilk777@gmail.com>
2022-06-21 10:32:59 +0300

    Initial commit.

---
+++ 20220615 - 17 - L - Lists Advanced/L17_07_the_office.py
@@ -0,0 +1,15 @@
+# 20220618 - Python Code - Lists Advance - Lecture
+# 07 - The Office - judge: https://judge.softuni.org/Contests/Practice/Index/1730#6
+
+
+emp_start_hap = list(map(int, input().split(' ')))
+factor = int(input())
+
+emp_hap_w_factor = [x * factor for x in emp_start_hap]
+average_hap = sum(emp_hap_w_factor) / len(emp_hap_w_factor)
+count_hap = len([x for x in emp_hap_w_factor if x >= average_hap])
+
+if count_hap >= len(emp_start_hap) / 2:
+    print(f'Score: {count_hap}/{len(emp_start_hap)}. Employees are happy!')
+else:
+    print(f'Score: {count_hap}/{len(emp_start_hap)}. Employees are not happy!')
