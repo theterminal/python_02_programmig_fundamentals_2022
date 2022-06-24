840626fefba19587d2fbf86f904b3b7c011e9733
theterminal <kirilk777@gmail.com>
2022-06-24 07:18:55 +0300

    Refactored code

--- 20220520 - 06 - E - Basic Syntax, Conditional Statement, Loops/L6_06_string_pureness.py
+++ 20220520 - 06 - E - Basic Syntax, Conditional Statement, Loops/L6_06_string_pureness.py
@@ -1,14 +1,16 @@
 # 20220521 - Python - Python Fundamentals - L6 - Basic Syntax, Conditional Statements, Loops
 # 06 - String Pureness - judge url: https://judge.softuni.org/Contests/Compete/Index/1719#5
 
+
 # ----------------- version 4 --------------------
-# This version is OK if there are many symbols to check within a string.
+
 
 num_n = int(input())
 lst_to_test = [',', '.', '_']
 
 for _ in range(num_n):
     str_to_test = input()
+
     for i in range(len(lst_to_test)):
         if lst_to_test[i] in str_to_test:
             print(f'{str_to_test} is not pure!')
@@ -16,23 +18,25 @@ for _ in range(num_n):
     else:
         print(f'{str_to_test} is pure.')
 
+
 # ----------------- version 3 --------------------
-# This version is OK if you have to check only few symbols
+
 
 num_n = int(input())
 
 for _ in range(num_n):
     str_to_test = input()
+
     if ',' in str_to_test or '.' in str_to_test or '_' in str_to_test:
         print(f'{str_to_test} is not pure!')
     else:
         print(f'{str_to_test} is pure.')
 
+
 # ----------------- version 2 --------------------
-# It goes only through the first special character, and it breaks!
+
 
 num_n = int(input())
-str_to_test = ""
 
 for i in range(num_n):
     str_to_test = input()
@@ -43,8 +47,9 @@ for i in range(num_n):
     else:
         print(f'{str_to_test} is pure.')
 
+
 # ------------------ version 1 -------------------
-# It goes through all the characters
+
 
 num_n = int(input())
 flag = False
