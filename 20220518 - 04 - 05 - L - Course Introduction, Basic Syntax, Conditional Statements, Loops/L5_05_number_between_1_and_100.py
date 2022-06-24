28de0c9e0cad44d5b86aefdd46278236ff412e8e
theterminal <kirilk777@gmail.com>
2022-06-24 07:16:33 +0300

    Refactored code

--- 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/L5_05_number_between_1_and_100.py
+++ 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/L5_05_number_between_1_and_100.py
@@ -1,7 +1,9 @@
 # 20220518 - Python - Python Fundamentals - L5 - Basic Syntax, Conditional Statements, Loops
 # 05 - Number Between 1 and 100 - judge url: https://judge.softuni.org/Contests/Practice/Index/1718#4
 
+
 # ----------- version 1 -------------------------------
+
 
 while True:
     num = float(input())
@@ -9,9 +11,13 @@ while True:
         print(f'The number {num} is between 1 and 100')
         break
 
+
 # ----------- version 2 --------------------------------
 
+
 num = float(input())
+
 while not (1 <= num <= 100):
     num = float(input())
+
 print(f'The number {num} is between 1 and 100')
