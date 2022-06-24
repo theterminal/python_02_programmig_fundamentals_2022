f63f9c68df126d8c5ee11c23c763daf149e80750
theterminal <kirilk777@gmail.com>
2022-06-24 07:17:21 +0300

    Refactored code

--- 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/L5_07_patterns_ver02.py
+++ 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/L5_07_patterns_ver02.py
@@ -1,7 +1,9 @@
 # 20220518 - Python - Python Fundamentals - L5 - Basic Syntax, Conditional Statements, Loops
 # 07 - Patterns - judge url: https://judge.softuni.org/Contests/Practice/Index/1718#6
 
+
 # ------------- version 2 -----------------------
+
 
 num_stars_max_per_row = int(input())
 
