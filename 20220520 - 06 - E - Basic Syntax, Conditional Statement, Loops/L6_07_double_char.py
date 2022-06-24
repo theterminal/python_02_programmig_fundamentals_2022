f62fe5f3ad8741f9dcdf3f859a0b8f4dbe6a2f45
theterminal <kirilk777@gmail.com>
2022-06-24 07:19:03 +0300

    Refactored code

--- 20220520 - 06 - E - Basic Syntax, Conditional Statement, Loops/L6_07_double_char.py
+++ 20220520 - 06 - E - Basic Syntax, Conditional Statement, Loops/L6_07_double_char.py
@@ -1,13 +1,37 @@
 # 20220521 - Python - Python Fundamentals - L6 - Basic Syntax, Conditional Statements, Loops
 # 07 - Double Char - judge url: https://judge.softuni.org/Contests/Compete/Index/1719#6
 
+
+# ---------- version 1 -----------------
+
+
 while True:
     str_to_test = input()
+
     if str_to_test == "End":
         break
+
     if str_to_test == "SoftUni":
         continue
 
     for j, k in enumerate(str_to_test):
         print(k * 2, end="")
+    print()
+
+
+# ---------- version 2 -----------------
+
+
+while True:
+    str_in = input()
+
+    if str_in == 'End':
+        break
+
+    if str_in == 'SoftUni':
+        continue
+
+    for i in str_in:
+        print(i * 2, end='')
+
     print()
