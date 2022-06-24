c04ca59af002ad046b09d1ce58206a22d7808414
theterminal <kirilk777@gmail.com>
2022-06-24 07:16:14 +0300

    Refactored code

--- 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/L5_03_word_reverse.py
+++ 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/L5_03_word_reverse.py
@@ -1,16 +1,21 @@
 # 20220518 - Python - Python Fundamentals - L5 - Basic Syntax, Conditional Statements, Loops
 # 03 - Word Reverse - judge url: https://judge.softuni.org/Contests/Practice/Index/1718#2
 
+
 # --------------------- version 1 ---------------------------
-word = input()
+
+
+word_1 = input()
 reversed_word = ''
 
-for i in range(len(word) - 1, -1, -1):
-    reversed_word += word[i]
+for i in range(len(word_1) - 1, -1, -1):
+    reversed_word += word_1[i]
 
 print(reversed_word)
 
+
 # --------------------- version 2 ---------------------------
+
 
 word_2 = input()
 print(word_2[::-1])
