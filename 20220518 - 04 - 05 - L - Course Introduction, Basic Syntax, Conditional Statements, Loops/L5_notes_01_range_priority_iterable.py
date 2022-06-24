1aff58cddaea373a3e05ff9df48fbed2b220cfa4
theterminal <kirilk777@gmail.com>
2022-06-24 07:17:29 +0300

    Refactored code

--- 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/L5_notes_01_range_priority_iterable.py
+++ 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/L5_notes_01_range_priority_iterable.py
@@ -1,13 +1,15 @@
 # 20220518 - Python - Python Fundamentals - L5 - Basic Syntax, Conditional Statements, Loops
-# Notes
-# This file is not intended for execution!
+# Notes 01
+
 
 # Storing range into variable
 a = range(1, 10)
 for i in a:
     print(i, end=", ")
 
+
 # -----------------------------------------------------------------
+
 
 # this example shows that 'not' has the highest priority
 if 2 > 1 or 3 < 4 and not 4 > 0:
@@ -17,10 +19,13 @@ if 2 > 1 or 3 < 4 and not 4 > 0:
 if (2 > 1 or 3 < 4) and not 4 > 0:
     print('This will NOT be printed')
 
+
 # -----------------------------------------------------------------
+
 
 # when iterable variable is not needed can use '_' only
 for _ in range(45):
     print(65)
+
 
 # -----------------------------------------------------------------
