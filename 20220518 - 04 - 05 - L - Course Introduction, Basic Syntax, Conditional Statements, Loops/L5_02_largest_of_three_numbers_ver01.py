3bf7eb1b937241cf7dec10dd4c6d258a56a21d15
theterminal <kirilk777@gmail.com>
2022-06-24 07:15:16 +0300

    Refactored code

--- 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/L5_02_largest_of_three_numbers_ver01.py
+++ 20220518 - 04 - 05 - L - Course Introduction, Basic Syntax, Conditional Statements, Loops/L5_02_largest_of_three_numbers_ver01.py
@@ -1,9 +1,13 @@
 # 20220518 - Python - Python Fundamentals - L5 - Basic Syntax, Conditional Statements, Loops
 # 02 - Largest of Three Numbers - judge url: https://judge.softuni.org/Contests/Practice/Index/1718#1
 
+
 # ------------------ version 1 ------------------------
 
-num_1, num_2, num_3 = int(input()), int(input()), int(input())
+
+num_1 = int(input())
+num_2 = int(input())
+num_3 = int(input())
 
 num_largest = max(num_1, num_2, num_3)
 print(num_largest)
