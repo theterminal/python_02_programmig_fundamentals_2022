882b45c90c0ba11bce8d4b77ffce4b507f380159
theterminal <kirilk777@gmail.com>
2022-06-25 07:15:52 +0300

    Refactored some code

--- 20220520 - 06 - E - Basic Syntax, Conditional Statement, Loops/L6_10_mutate_strings.py
+++ 20220520 - 06 - E - Basic Syntax, Conditional Statement, Loops/L6_10_mutate_strings.py
@@ -1,14 +1,35 @@
 # 20220521 - Python - Python Fundamentals - L6 - Basic Syntax, Conditional Statements, Loops
 # 10 - Mutate Strings - judge url: https://judge.softuni.org/Contests/Compete/Index/1719#9
 
+
+# ------------------- version 3 -------------------------------- KK
+
+
+str_1 = input()
+str_2 = input()
+
+for i in range(len(str_1)):
+    if str_2[i] == str_1[i]:
+        continue
+
+    part_l = str_2[:i + 1]
+    part_r = str_1[i + 1:len(str_1)]
+    result = part_l + part_r
+
+    print(result)
+
+
 # ------------------- version 2 -------------------------------- KK
 
-str_1, str_2 = input(), input()
+
+str_1 = input()
+str_2 = input()
+
 last_str_printed = str_1
 
 for i in range(len(str_1)):
-    part_l = str_2[: i + 1]
-    part_r = str_1[i + 1: len(str_1)]
+    part_l = str_2[:i + 1]
+    part_r = str_1[i + 1:len(str_1)]
     word_to_print = part_l + part_r
 
     if last_str_printed == word_to_print:
@@ -17,9 +38,12 @@ for i in range(len(str_1)):
         last_str_printed = word_to_print
         print(last_str_printed)
 
+
 # ------------------- version 1 -------------------------------- KK
 
-str_1, str_2 = input(), input()
+
+str_1 = input()
+str_2 = input()
 word_to_print = ''
 
 for i in range(len(str_1)):
