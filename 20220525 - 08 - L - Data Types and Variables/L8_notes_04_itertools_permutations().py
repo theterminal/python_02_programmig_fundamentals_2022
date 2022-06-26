821eb33edf5c7a93823f852c4396774f260b1dcc
theterminal <kirilk777@gmail.com>
2022-06-26 13:26:16 +0300

    Refactored some code

--- 20220525 - 08 - L - Data Types and Variables/L8_notes_04_itertools_permutations().py
+++ 20220525 - 08 - L - Data Types and Variables/L8_notes_04_itertools_permutations().py
@@ -9,48 +9,61 @@ from itertools import permutations
 # -------------------------------------------
 print('\n--------- Example 1 ------------\n')
 
+
 for i in permutations((1, 2, 3)):               # 'r' parameter can be omitted, and it equals to the max value
     print(i)
 
+
 # -------------------------------------------
 print('\n--------- Example 2 ------------\n')
+
 
 tup_1 = (1, 2, 3)
 for i in permutations(tup_1, 3):                # 'r' parameter cannot exceed the number of elements in the tuple!
     print(i)
 
+
 # -------------------------------------------
 print('\n--------- Example 3 ------------\n')
+
 
 tup_1 = (1, 2, 3, 4, 5)
 
 for i in permutations(tup_1, len(tup_1) - 1):   # 'r' parameter can be a formula
     print(i)
 
+
 # -------------------------------------------
 print('\n--------- Example 4 ------------\n')
+
 
 tup_1 = (1, 2, 3)
 all_permutations = permutations(tup_1, 3)       # another syntax
 for i in all_permutations:
     print(i)
 
+
 # -------------------------------------------
 print('\n--------- Example 5 ------------\n')
+
 
 print('\nPermutations from Tuple')
 for i in permutations((1, 2, 3), 3):        # the correct syntax
     print(i, end=f"   ->   {type(i)}")
     print()
 
+
 # -------------------------------------------
+
 
 print('\nPermutations from List')
 for i in permutations([1, 2, 3], 3):        # this works fine but syntax should be as Tuple
     print(i, end=f"   ->   {type(i)}")
     print()
 
+
 # -------------------------------------------
+
 
 print('\nPermutations from Set')
 for i in permutations({1, 2, 3}, 3):        # this works fine but syntax should be as Tuple
