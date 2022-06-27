4ed92b78d9c814a2873beee192c41cb5c92e014b
theterminal <kirilk777@gmail.com>
2022-06-27 16:22:45 +0300

    Refactored some code

--- 20220527 - 09 - E - Data Types and Variables/L9_more_03_decrypting_messages.py
+++ 20220527 - 09 - E - Data Types and Variables/L9_more_03_decrypting_messages.py
@@ -1,7 +1,6 @@
 # 20220528 - Python - Python Fundamentals - L9 - Data Types and Variables
 # More Exercises 03 - Decrypting Messages - judge url: https://judge.softuni.org/Contests/Practice/Index/1723#2
 
-# ------- version 2 ----------
 
 key = int(input())
 num_lines = range(int(input()))
@@ -9,17 +8,5 @@ str_to_print = ''
 
 for _ in num_lines:
     str_to_print += chr(ord(input()) + key)
-
-print(str_to_print)
-
-# ------- version 1 -----------
-
-key = int(input())
-num_lines = range(int(input()))
-str_to_print = ''
-
-for _ in num_lines:
-    char = ord(input())
-    str_to_print += chr(char + key)
 
 print(str_to_print)
