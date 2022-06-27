716f58445f07dd81bef2808e72e15bb1f0ff5792
theterminal <kirilk777@gmail.com>
2022-06-27 16:22:22 +0300

    Refactored some code

--- 20220527 - 09 - E - Data Types and Variables/L9_05_print_part_of_the_ascii_table.py
+++ 20220527 - 09 - E - Data Types and Variables/L9_05_print_part_of_the_ascii_table.py
@@ -1,7 +1,9 @@
 # 20220527 - Python - Python Fundamentals - L9 - Data Types and Variables
 # 05 - Print Part Of The ASCII Table - judge url: https://judge.softuni.org/Contests/Compete/Index/1722#4
 
+
 # ------------- version 2 --------------------
+
 
 index_start = int(input())
 index_end = int(input())
@@ -13,7 +15,9 @@ for i in range(index_start, index_end + 1):
 
 print(result.strip())                           # .strip() takes out the white space from both sides of the string
 
+
 # ------------- version 1 --------------------
+
 
 index_start = int(input())
 index_end = int(input())
@@ -28,7 +32,9 @@ for i in range(index_start, index_end + 1):
 
 print(result)
 
+
 # ------------- version 3 --------------------
+
 
 index_start = int(input())
 index_end = int(input())
