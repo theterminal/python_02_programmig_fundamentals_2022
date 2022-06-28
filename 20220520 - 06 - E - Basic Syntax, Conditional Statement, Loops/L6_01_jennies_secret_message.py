06cf28132d8dc9cd1114835e73e569eb8e1d9c9c
theterminal <kirilk777@gmail.com>
2022-06-28 07:11:33 +0300

    Refactored some code

--- 20220520 - 06 - E - Basic Syntax, Conditional Statement, Loops/L6_01_jennies_secret_message.py
+++ 20220520 - 06 - E - Basic Syntax, Conditional Statement, Loops/L6_01_jennies_secret_message.py
@@ -1,6 +1,7 @@
 # 20220520 - Python - Python Fundamentals - L6 - Basic Syntax, Conditional Statements, Loops
 # 01 - Jenny's Secret Message - judge url: https://judge.softuni.org/Contests/Compete/Index/1719#0
 
+
 name_entered = input()
 
 if name_entered == 'Johnny':
