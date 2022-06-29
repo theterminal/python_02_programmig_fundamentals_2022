fdc19228d3c6ae6dbec4fd4be5d6d662f0806786
theterminal <kirilk777@gmail.com>
2022-06-29 17:27:05 +0300

    Refactored some code

--- 20220525 - 08 - L - Data Types and Variables/L8_02_centuries_to_minutes.py
+++ 20220525 - 08 - L - Data Types and Variables/L8_02_centuries_to_minutes.py
@@ -1,7 +1,9 @@
 # 20220529 - Python - Python Fundamentals - L9 - Data Types and Variables
 # 02 - Centuries to Minutes - judge url: https://judge.softuni.org/Contests/Practice/Index/1721#1
 
+
 # ------------------ version 2 ---------------------------
+
 
 from decimal import Decimal
 
@@ -14,7 +16,9 @@ minutes = hours * 60
 
 print(f'{centuries_entered} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes')
 
+
 # ------------------ version 1 ---------------------------
+
 
 centuries_entered = int(input())
 
