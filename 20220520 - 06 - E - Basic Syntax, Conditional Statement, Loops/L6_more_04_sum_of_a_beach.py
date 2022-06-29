7f47900b4a34b0e130b46dfb35feb7127712c7cb
theterminal <kirilk777@gmail.com>
2022-06-29 17:23:07 +0300

    Refactored some code

--- 20220520 - 06 - E - Basic Syntax, Conditional Statement, Loops/L6_more_04_sum_of_a_beach.py
+++ 20220520 - 06 - E - Basic Syntax, Conditional Statement, Loops/L6_more_04_sum_of_a_beach.py
@@ -1,7 +1,9 @@
 # 20220528 - Python - Python Fundamentals - L6 - Basic Syntax, Conditional Statements, Loops
 # More Exercise 04 - Sum of a Beach - judge url: https://judge.softuni.org/Contests/Practice/Index/1720#3
 
+
 # --------------------------- version 2 ---------------------------------
+
 
 str_entered = input().lower()
 words_to_find = ['sand', 'water', 'fish', 'sun']
@@ -12,13 +14,15 @@ for i in range(len(words_to_find)):
 
 print(num_of_occurrences)
 
+
 # --------------------------- version 1 ---------------------------------
 
-# str_entered = input().lower()
-#
-# result = str_entered.count('sand')
-# result += str_entered.count('water')
-# result += str_entered.count('fish')
-# result += str_entered.count('sun')
-#
-# print(result)
+
+str_entered = input().lower()
+
+result = str_entered.count('sand')
+result += str_entered.count('water')
+result += str_entered.count('fish')
+result += str_entered.count('sun')
+
+print(result)
