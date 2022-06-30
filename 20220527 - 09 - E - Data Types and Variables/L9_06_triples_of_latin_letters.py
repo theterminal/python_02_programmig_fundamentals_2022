146ea9ca7b3a02745ea87af5d28f3017cbfe2074
theterminal <kirilk777@gmail.com>
2022-06-30 16:11:21 +0300

    Refactored some code

--- 20220527 - 09 - E - Data Types and Variables/L9_06_triples_of_latin_letters.py
+++ 20220527 - 09 - E - Data Types and Variables/L9_06_triples_of_latin_letters.py
@@ -1,6 +1,7 @@
 # 20220527 - Python - Python Fundamentals - L9 - Data Types and Variables
 # 06 - Triples Of Latin Letters - judge url: https://judge.softuni.org/Contests/Compete/Index/1722#5
 
+
 num_letters = int(input())
 ascii_range = range(97, 97 + num_letters)
 
