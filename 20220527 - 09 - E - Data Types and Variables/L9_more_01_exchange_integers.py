9a9d8aed788bcea95c1b266b4406b75255241f85
theterminal <kirilk777@gmail.com>
2022-06-30 16:12:20 +0300

    Refactored some code

--- 20220527 - 09 - E - Data Types and Variables/L9_more_01_exchange_integers.py
+++ 20220527 - 09 - E - Data Types and Variables/L9_more_01_exchange_integers.py
@@ -1,7 +1,9 @@
 # 20220528 - Python - Python Fundamentals - L9 - Data Types and Variables
 # More Exercises 01 - Exchange Integers - judge url: https://judge.softuni.org/Contests/Practice/Index/1723#0
 
+
 # ---------------- version 2 ------------------
+
 
 num_1, num_2 = int(input()), int(input())
 
@@ -13,7 +15,9 @@ num_2 = num_temp
 
 print(f'After:\na = {num_1}\nb = {num_2}')
 
+
 # ---------------- version 1 ------------------ judge gives 100% but logically is NOT correct
+
 
 num_1, num_2 = int(input()), int(input())
 
