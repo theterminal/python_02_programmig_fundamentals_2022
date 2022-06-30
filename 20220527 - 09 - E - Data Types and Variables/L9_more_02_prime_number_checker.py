e6bc3bea49279b055fb17b9542db39c8d9522c9b
theterminal <kirilk777@gmail.com>
2022-06-30 16:12:53 +0300

    Refactored some code

--- 20220527 - 09 - E - Data Types and Variables/L9_more_02_prime_number_checker.py
+++ 20220527 - 09 - E - Data Types and Variables/L9_more_02_prime_number_checker.py
@@ -1,6 +1,7 @@
 # 20220528 - Python - Python Fundamentals - L9 - Data Types and Variables
 # More Exercises 02 - Prime Number Checker - judge url: https://judge.softuni.org/Contests/Practice/Index/1723#1
 
+
 num = int(input())
 
 if num <= 1:
