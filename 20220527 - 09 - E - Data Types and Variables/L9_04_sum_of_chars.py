1d313383e35f1581be84580c654a050ce35a59ca
theterminal <kirilk777@gmail.com>
2022-06-30 16:10:54 +0300

    Refactored some code

--- 20220527 - 09 - E - Data Types and Variables/L9_04_sum_of_chars.py
+++ 20220527 - 09 - E - Data Types and Variables/L9_04_sum_of_chars.py
@@ -1,7 +1,9 @@
 # 20220527 - Python - Python Fundamentals - L9 - Data Types and Variables
 # 04 - Sum of Chars - judge url: https://judge.softuni.org/Contests/Compete/Index/1722#3
 
+
 # --------- version 1 ---------------------
+
 
 num_chars = int(input())
 sum_of_ascii = 0
@@ -12,7 +14,9 @@ for i in range(num_chars):
 
 print(f'The sum equals: {sum_of_ascii}')
 
+
 # ---------- version 2 --------------------
+
 
 num_chars = range(int(input()))
 sum_of_ascii = 0
