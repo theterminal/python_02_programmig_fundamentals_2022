dd021d107cace4afc18cd87778728b0ac62ff320
theterminal <kirilk777@gmail.com>
2022-07-01 05:56:17 +0300

    Refactored some code

--- 20220601 - 11 - L - Lists - Basics/L11_02_courses.py
+++ 20220601 - 11 - L - Lists - Basics/L11_02_courses.py
@@ -1,6 +1,10 @@
 # 20220601 - Python Code - L11 - Lists - Basics
 # 02 - Courses - judge url: https://judge.softuni.org/Contests/Practice/Index/1724#1
 
+
+# ---------- version 1 ---------------------
+
+
 num_lines = int(input())
 course_list = list()
 
@@ -10,7 +14,9 @@ for _ in range(num_lines):
 
 print(course_list)
 
+
 # ---------- version 2 ---------------------
+
 
 num_lines = int(input())
 course_list = list()
@@ -20,16 +26,3 @@ for _ in range(num_lines):
     course_list += [course_name_entered]
 
 print(course_list)
-
-# ---------- something else interesting ----
-
-num_lines = int(input())
-course_list = list()
-
-for _ in range(num_lines):
-    course_name_entered = input()
-    course_list += course_name_entered
-
-print(course_list)
-print(type(course_list))
-print(len(course_list))
