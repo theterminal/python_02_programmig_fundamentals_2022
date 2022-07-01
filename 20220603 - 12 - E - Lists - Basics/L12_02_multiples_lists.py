7818793ff29d774d9568785edf87045e6311d67c
theterminal <kirilk777@gmail.com>
2022-07-01 05:59:23 +0300

    Refactored some code

--- 20220603 - 12 - E - Lists - Basics/L12_02_multiples_lists.py
+++ 20220603 - 12 - E - Lists - Basics/L12_02_multiples_lists.py
@@ -1,7 +1,9 @@
 # 20220603 - Python Code - L12 - Lists Basics - Exercise
 # 02 - Multiple Lists - judge url: https://judge.softuni.org/Contests/Compete/Index/1725#1
 
+
 # ----------------- version 1 -------------------------------
+
 
 factor = int(input())
 count = int(input())
@@ -19,6 +21,7 @@ print(list_1)
 
 # ----------------- version 2 -------------------------------
 
+
 factor = int(input())
 count = int(input())
 
@@ -29,11 +32,20 @@ for i in range(1, count + 1):
 
 print(list_1)
 
+
 # -------------------- version 3 -----------------------------
+
 
 factor = int(input())
 count = int(input())
 
-list_1 = list(range(factor, count * factor + 1, factor))
-print(list_1)
+print(list(range(factor, count * factor + 1, factor)))
 
+
+# -------------------- version 4 -----------------------------
+
+
+factor = int(input())
+count = int(input())
+
+print([factor * i for i in range(1, count + 1)])
