08aa1d96fb137cff463dcbc3668891e4ba6e94f1
theterminal <kirilk777@gmail.com>
2022-07-01 06:00:28 +0300

    Refactored some code

--- 20220603 - 12 - E - Lists - Basics/L12_04_number_beggars.py
+++ 20220603 - 12 - E - Lists - Basics/L12_04_number_beggars.py
@@ -1,59 +1,36 @@
 # 20220603 - Python Code - L12 - Lists Basics - Exercise
 # 04 - Number Beggars - judge url: https://judge.softuni.org/Contests/Compete/Index/1725#3
 
-list_str_entered = input().split(', ')
+
+# ---------------------- version 1 ---------------------------------
+
+
+lst_nums = list(map(int, input().split(', ')))
 num_beggars = int(input())
 
-list_sums_print = []
-current_beggar = 0
+lst_out = []
 
-# list_int = [int(i) for i in list_str]
-
-list_int = []
-
-for element in list_str_entered:
-    list_int.append(int(element))
-
-while current_beggar < num_beggars:
+for beggar in range(num_beggars):
     sum_current_beggar = 0
 
-    for current_index in range(current_beggar, len(list_int), num_beggars):
-        sum_current_beggar += list_int[current_index]
+    for num in range(beggar, len(lst_nums), num_beggars):
+        sum_current_beggar += lst_nums[num]
 
-    current_beggar += 1
-    list_sums_print.append(sum_current_beggar)
+    lst_out.append(sum_current_beggar)
 
-print(list_sums_print)
-
-# ----------- someone else ----------------
-
-list_money = input().split(", ")
-num_beggars = int(input())
-
-list_sums_current_beggar = []
-list_totals_all_beggars = []
-
-for current_beggar in range(num_beggars):
-
-    for index in range(current_beggar, len(list_money), num_beggars):
-        list_sums_current_beggar.append(int(list_money[index]))
-
-    list_totals_all_beggars.append(sum(list_sums_current_beggar))
-    list_sums_current_beggar.clear()
-
-print(list_totals_all_beggars)
+print(lst_out)
 
 
-# ----------- someone else ----------------
+# ---------------------- version 2 ---------------------------------
 
-collect = input()
-collectors = int(input())
 
-collect_list = list(map(int, collect.split(", ")))
-collector_final = []
+lst_collect = list(map(int, input().split(', ')))
+num_collectors = int(input())
 
-for i in range(collectors):
-    middle_list = collect_list[i::collectors]
-    collector_final.append(sum(middle_list))
+lst_collector_final = []
 
-print(collector_final)
+for collector in range(num_collectors):
+    middle_list = lst_collect[collector::num_collectors]
+    lst_collector_final.append(sum(middle_list))
+
+print(lst_collector_final)
