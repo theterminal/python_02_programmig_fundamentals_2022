3aad49e577c0da534c8380f0f58b2676a834f77e
theterminal <kirilk777@gmail.com>
2022-07-01 05:58:45 +0300

    Refactored some code

--- 20220603 - 12 - E - Lists - Basics/L12_01_invert_values.py
+++ 20220603 - 12 - E - Lists - Basics/L12_01_invert_values.py
@@ -1,6 +1,16 @@
 # 20220603 - Python Code - L12 - Lists Basics - Exercise
 # 01 - Invert Values - judge url: https://judge.softuni.org/Contests/Compete/Index/1725#0
 
+
+# ---------------------- version 2 -----------------------
+
+
+print(list(map(lambda x: int(x) * -1, input().split(' '))))
+
+
+# ---------------------- version 1 -----------------------
+
+
 list_start_values = input().split(' ')
 list_opposite_values = list()
 
