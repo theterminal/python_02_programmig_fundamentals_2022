f2e19acc8033a7a19eaa259e31e72af98dca60fe
theterminal <kirilk777@gmail.com>
2022-07-02 09:16:46 +0300

    Refactored some code

--- 20220603 - 12 - E - Lists - Basics/L12_05_faro_shuffle.py
+++ 20220603 - 12 - E - Lists - Basics/L12_05_faro_shuffle.py
@@ -1,24 +1,24 @@
 # 20220603 - Python Code - L12 - Lists Basics - Exercise
 # 05 - Faro Shuffle - judge url: https://judge.softuni.org/Contests/Compete/Index/1725#4
 
+
 lst_cards_given = input().split(' ')
 num_shuffles = int(input())
 
 len_cards_given_dev_2 = int(len(lst_cards_given) / 2)               # can use 'len // 2' and it'll be no problem for int!
 lst_cards_to_work_with = lst_cards_given.copy()
-lst_to_print = []
-
+lst_to_shuffle = []
 
 while num_shuffles > 0:
     lst_left = lst_cards_to_work_with[:len_cards_given_dev_2]
     lst_right = lst_cards_to_work_with[len_cards_given_dev_2::]
 
     for i in range(len_cards_given_dev_2):
-        lst_to_print.append(lst_left[i])
-        lst_to_print.append(lst_right[i])
+        lst_to_shuffle.append(lst_left[i])
+        lst_to_shuffle.append(lst_right[i])
 
-    lst_cards_to_work_with = lst_to_print.copy()
-    lst_to_print.clear()
+    lst_cards_to_work_with = lst_to_shuffle.copy()
+    lst_to_shuffle.clear()
     num_shuffles -= 1
 
 print(lst_cards_to_work_with)
