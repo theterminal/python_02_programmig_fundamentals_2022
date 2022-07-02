7b6a2aa89cbcc6540b4767240763d9aac7ebbdd4
theterminal <kirilk777@gmail.com>
2022-07-02 09:19:45 +0300

    Refactored some code

--- 20220603 - 12 - E - Lists - Basics/L12_more_02_messaging.py
+++ 20220603 - 12 - E - Lists - Basics/L12_more_02_messaging.py
@@ -1,51 +1,23 @@
 # 22020613 - Python Code - L12 - Lists Basics - More Exercise
 # 02 - Messaging - judge url: https://judge.softuni.org/Contests/Practice/Index/1726#1
 
-str_1 = input().split()
-str_2 = input()
 
-sum_nums = sum_sub = index_model = 0
+nums_in = input().split(' ')
+str_in = input()
 
-# code for the digits part
-for i in range(len(str_1)):
-    for j in range(len(str_1[i])):
-        num = int(str_1[i][j])
-        sum_sub += num
-    index_model += sum_sub
-    sum_sub = 0
-print(index_model, '- index model')
+index = 0
+message = ''
 
-# code for the string part
-lst_from_str_2 = list()
-count_char = len(str_2)
-print(count_char, '- counter_char')
+for i in nums_in:
+    for j in range(len(i)):
+        index += int(i[j])
 
-for j in str_2:
-    lst_from_str_2.append(j)
+    while index > len(str_in):
+        index -= len(str_in)
 
-print(lst_from_str_2)
+    message += str_in[index]
+    str_in = str_in[:index] + str_in[index + 1:]
 
-message = list()
-index_to_print = 0
-index_match = 0
-flag = False
-out = 0
-
-while len(lst_from_str_2) > 0:
-    while True:
-        if index_match == index_model:
-            break
-        else:
-            index_match += 1
-
-        index_to_print += 1
-        if index_to_print == len(lst_from_str_2):
-            index_to_print = 0
-
-    message.append(lst_from_str_2[index_to_print])
-    lst_from_str_2.pop(index_to_print)
-    index_match = 0
-    index_to_print = 0
+    index = 0
 
 print(message)
-
