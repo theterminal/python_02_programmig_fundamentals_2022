0b0ac76ec71c8d26568b8a5f31c867a6b975621e
theterminal <kirilk777@gmail.com>
2022-07-02 09:18:49 +0300

    Refactored some code

--- 20220603 - 12 - E - Lists - Basics/L12_09_hello_france.py
+++ 20220603 - 12 - E - Lists - Basics/L12_09_hello_france.py
@@ -1,6 +1,7 @@
 # 22020613 - Python Code - L12 - Lists Basics - Exercise
 # 09 - Hello France - https://judge.softuni.org/Contests/Compete/Index/1725#8
 
+
 train_ticket = 150
 input_str = input().split('|')             # [0.00:1000.00]
 budget_in = float(input())                 # [0.0:1000.0]
@@ -17,28 +18,24 @@ for i in range(len(input_str)):
     item = input_str[i].split('->')
     price_in = float(item[1])
 
-    if 'Clothes' in item and price_in <= max_price_clothes:
-        if budget_left >= price_in:
-            budget_left -= price_in
-            lst_new_prices.append(f'{(price_in * 1.4):.2f}')
-            budget_out += price_in * 1.4
+    if 'Clothes' in item and price_in <= max_price_clothes and budget_left >= price_in:
+        budget_left -= price_in
+        lst_new_prices.append(f'{(price_in * 1.4):.2f}')
+        budget_out += price_in * 1.4
 
-    elif 'Shoes' in item and price_in <= max_price_shoes:
-        if budget_left >= price_in:
-            budget_left -= price_in
-            lst_new_prices.append(f'{(price_in * 1.4):.2f}')
-            budget_out += price_in * 1.4
+    elif 'Shoes' in item and price_in <= max_price_shoes and budget_left >= price_in:
+        budget_left -= price_in
+        lst_new_prices.append(f'{(price_in * 1.4):.2f}')
+        budget_out += price_in * 1.4
 
-    elif 'Accessories' in item and price_in <= max_price_accessories:
-        if budget_left >= price_in:
-            budget_left -= price_in
-            lst_new_prices.append(f'{(price_in * 1.4):.2f}')
-            budget_out += price_in * 1.4
+    elif 'Accessories' in item and price_in <= max_price_accessories and budget_left >= price_in:
+        budget_left -= price_in
+        lst_new_prices.append(f'{(price_in * 1.4):.2f}')
+        budget_out += price_in * 1.4
 
 profit = budget_out - budget_in + budget_left
 budget_final = budget_in + profit
 
-lst_str_new_prices = [str(i) for i in lst_new_prices]
 print(' '.join(lst_new_prices))
 print(f'Profit: {profit:.2f}')
 
