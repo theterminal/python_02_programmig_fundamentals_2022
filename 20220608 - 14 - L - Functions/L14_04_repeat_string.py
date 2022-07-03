fda764b4b070a13d1308ddb084f7473f4c31e5a7
theterminal <kirilk777@gmail.com>
2022-07-03 07:43:35 +0300

    Refactored code

--- 20220608 - 14 - L - Functions/L14_04_repeat_string.py
+++ 20220608 - 14 - L - Functions/L14_04_repeat_string.py
@@ -5,24 +5,23 @@
 # ------- normal function ---------------------
 
 
-# def repeat_string(str_a1, counter_a2):
-#     result = str_a1 * counter_a2
-#
-#     return result
-#
-#
-# a1 = input()
-# a2 = int(input())
-#
-# print(repeat_string(a1, a2))
+def repeat_string(str_a1, counter_a2):
+    result = str_a1 * counter_a2
+
+    return result
+
+
+a1 = input()
+a2 = int(input())
+
+print(repeat_string(a1, a2))
 
 
 # ------- using lambda function ----------------
 
 
-result_2 = lambda x, y: x * y
-
 x1 = input()
 counter_y = int(input())
 
+result_2 = lambda x, y: x * y
 print(result_2(x1, counter_y))
