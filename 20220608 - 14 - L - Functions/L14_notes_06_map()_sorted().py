597ca9bbee04408f14fabbc0bd7f95581088eb39
theterminal <kirilk777@gmail.com>
2022-07-04 08:22:48 +0300

    Refactored code

--- 20220608 - 14 - L - Functions/L14_notes_06_map()_sorted().py
+++ 20220608 - 14 - L - Functions/L14_notes_06_map()_sorted().py
@@ -1,10 +1,11 @@
 # 20220608 - Python Code - Functions - Lecture
 # 06 - map(), sorted()
 
+
 # ------- input str of numbers mapped to integers in a list --------
-
-
 print('\n------- enter the string of numbers -------\n')
+
+
 # must enter the numbers: 1, 2, 3, 4, 5, 6, 7
 
 numbers = list(map(int, input().split(', ')))
@@ -18,9 +19,8 @@ for i in numbers:
 
 
 # ------------------- number on a power ---------------------------
-
-
 print('\n------- power of nums with function and map() -------\n')
+
 
 numbers = [1, 2, 3, 4, 5, 6, 7]
 
@@ -32,10 +32,10 @@ def power_of_nums(num):
 powers = list(map(power_of_nums, numbers))
 print(powers)
 
+
 # ------------------- sorted() -----------------------------------
-
-
 print('\n------- sorted() -------------------------------------\n')
+
 
 numbers = [19, -4, 13, 0, 5, 7, 7]
 print(sorted(numbers))
