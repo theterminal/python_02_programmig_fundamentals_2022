c27c73c960c9bc984a63c521405d3c7d078a24dd
theterminal <kirilk777@gmail.com>
2022-07-04 08:26:16 +0300

    Refactored code

--- 20220610 - 15 - E - Functions/L15_02_add_and_subtract.py
+++ 20220610 - 15 - E - Functions/L15_02_add_and_subtract.py
@@ -1,6 +1,7 @@
 # 20220614 - Python Code - Functions - Exercise
 # 02 - Add and Subtract - judge url: https://judge.softuni.org/Contests/Compete/Index/1728#1
 
+
 # ------------------ version 3 ------------------
 
 
@@ -26,18 +27,17 @@ print(add_and_subtract(z_1, z_2, z_3))
 # ------------------ version 2 ------------------
 
 
-def sum_numbers(z1, z2):
+def sum_nums(z1, z2):
     return z1 + z2
 
 
-def subtract(sum_z1_z2, z3):
+def subtract_2(sum_z1_z2, z3):
     return sum_z1_z2 - z3
 
 
 def add_and_subtract(z1, z2, z3):
-    sum_z1_z2 = sum_numbers(z1, z2)
-    result = subtract(sum_z1_z2, z3)
-    return result
+    sum_z1_z2 = sum_nums(z1, z2)
+    return subtract_2(sum_z1_z2, z3)
 
 
 z_1 = int(input())
@@ -50,17 +50,17 @@ print(add_and_subtract(z_1, z_2, z_3))
 # ------------------ version 1 ------------------
 
 
-def sum_numbers(x1, x2):
+def sum_nums_2(x1, x2):
     return x1 + x2
 
 
-def subtract(sum_x1_plus_x2, x3):
+def subtract_3(sum_x1_plus_x2, x3):
     return sum_x1_plus_x2 - x3
 
 
 def add_and_subtract(z1, z2, z3):
-    sum_of_1_and_2 = sum_numbers(z1, z2)
-    result = subtract(sum_of_1_and_2, z3)
+    sum_of_1_and_2 = sum_nums_2(z1, z2)
+    result = subtract_3(sum_of_1_and_2, z3)
     return result
 
 
