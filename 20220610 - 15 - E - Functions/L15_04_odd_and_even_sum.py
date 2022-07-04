83c611e2dc768ef6ebab6aff2a4a591a460498ec
theterminal <kirilk777@gmail.com>
2022-07-04 08:26:47 +0300

    Refactored code

--- 20220610 - 15 - E - Functions/L15_04_odd_and_even_sum.py
+++ 20220610 - 15 - E - Functions/L15_04_odd_and_even_sum.py
@@ -1,20 +1,19 @@
 # 20220614 - Python Code - Functions - Exercise
 # 04 - Odd and Even Sum - judge url: https://judge.softuni.org/Contests/Compete/Index/1728#3
 
-def odd_and_even_sums(z1):
-    str_num = str(z1)
 
+def odd_and_even_sums(z1):
     sum_even = 0
     sum_odd = 0
 
-    for i in range(len(str_num)):
-        if int(str_num[i]) % 2 == 0:
-            sum_even += int(str_num[i])
+    for i in range(len(z1)):
+        if int(z1[i]) % 2 == 0:
+            sum_even += int(z1[i])
         else:
-            sum_odd += int(str_num[i])
+            sum_odd += int(z1[i])
 
     print(f'Odd sum = {sum_odd}, Even sum = {sum_even}')
 
 
-num_in = int(input())
+num_in = input()
 odd_and_even_sums(num_in)
