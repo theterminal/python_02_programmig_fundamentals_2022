644181bed182aaafc94dac48a33ecfb7fd7f8e88
theterminal <kirilk777@gmail.com>
2022-07-05 08:13:41 +0300

    Refactored code

--- 20220610 - 15 - E - Functions/L15_09_password_validator.py
+++ 20220610 - 15 - E - Functions/L15_09_password_validator.py
@@ -1,7 +1,9 @@
 # 20220614 - Python Code - Functions - Exercise
 # 09 - Password Validator - judge url: https://judge.softuni.org/Contests/Compete/Index/1728#8
 
+
 # -------------- version 3 --- using 3 functions, .isalnum(), .isdigit(), validate all ------------------
+
 
 def length_is_valid(some_string):
     if 6 <= len(some_string) <= 10:
@@ -42,17 +44,16 @@ if all(validate):
 
 
 def pass_validator(test_in: str):
-    flag = False
 
     # Length of the password must be [6:10]
     if not (6 <= len(test_in) <= 10):
         print('Password must be between 6 and 10 characters')
-        flag = True
+        return
 
     # Only alphanumeric chars must be used
     if not (test_in.isalnum()):
         print('Password must consist only of letters and digits')
-        flag = True
+        return
 
     # Must contain at leas 2 digits
     count_digits = 0
@@ -63,13 +64,9 @@ def pass_validator(test_in: str):
     else:
         if count_digits < 2:
             print('Password must have at least 2 digits')
-            flag = True
+            return
 
-    # flag test
-    if flag:
-        return
-    else:
-        print('Password is valid')
+    print('Password is valid')
 
 
 pass_in = input()
@@ -80,19 +77,18 @@ pass_validator(pass_in)
 
 
 def pass_validator(test_in: str):
-    flag = False
 
     # Length of the password must be [6:10]
     if not (6 <= len(test_in) <= 10):
         print('Password must be between 6 and 10 characters')
-        flag = True
+        return
 
     # Only alphanumeric chars must be used
     for i in test_in:
         if not (48 <= ord(i) <= 57 or 65 <= ord(i) <= 90 or 97 <= ord(i) <= 122):
             print('Password must consist only of letters and digits')
             flag = True
-            break
+            return
 
     # Must contain at leas 2 digits
     count_digits = 0
@@ -103,13 +99,9 @@ def pass_validator(test_in: str):
     else:
         if count_digits < 2:
             print('Password must have at least 2 digits')
-            flag = True
+            return
 
-    # flag test
-    if flag:
-        return
-    else:
-        print('Password is valid')
+    print('Password is valid')
 
 
 pass_in = input()
