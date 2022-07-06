0b3ff4326b8c830ceb5659b942bbaf2c0763991f
theterminal <kirilk777@gmail.com>
2022-07-06 08:28:32 +0300

    Refactored code

--- 20220615 - 17 - L - Lists Advanced/L17_04_palindrome_strings.py
+++ 20220615 - 17 - L - Lists Advanced/L17_04_palindrome_strings.py
@@ -1,26 +1,28 @@
 # 20220617 - Python Code - Lists Advance - Lecture
 # 04 - Palindrome Strings - judge: https://judge.softuni.org/Contests/Practice/Index/1730#3
 
+
 # ----------------- version 1 -------------------------
+
 
 words_in = input().split(' ')
 palindrome_in = input()
-
 palindrome_out = []
+
 for i in words_in:
     if i[:int(int(len(i)) / 2)] == i[-1: int(len(i) / 2):-1]:           # if i == i[::-1] - it is a palindrome
         palindrome_out.append(i)
-
-print(palindrome_out)
 
 number = 0
 if palindrome_in in palindrome_out:
     number = palindrome_out.count(palindrome_in)
 
+print(palindrome_out)
 print(f'Found palindrome {number} times')
 
 
 # ----------------- version 2 -------------------------
+
 
 def palindrome_filter(x):
     if x == x[::-1]:
