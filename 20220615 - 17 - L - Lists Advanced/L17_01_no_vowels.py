11b82450487c8b29055f7d8f45eabbc37426d832
theterminal <kirilk777@gmail.com>
2022-07-06 08:27:50 +0300

    Refactored code

--- 20220615 - 17 - L - Lists Advanced/L17_01_no_vowels.py
+++ 20220615 - 17 - L - Lists Advanced/L17_01_no_vowels.py
@@ -1,9 +1,10 @@
 # 20220616 - Python Code - Lists Advance - Lecture
 # 01 - No Vowels - judge: https://judge.softuni.org/Contests/Practice/Index/1730#0
 
+
 str_in = input()
 
 nots = ['a', 'o', 'u', 'e', 'i']
-result = [n for n in str_in if n.lower() not in nots]
+result = [i for i in str_in if i.lower() not in nots]
 
 print(''.join(result))
