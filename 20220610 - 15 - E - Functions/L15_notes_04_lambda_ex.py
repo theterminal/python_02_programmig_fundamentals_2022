e5fae3b0f8ca6cb1d145743f561cde771bd2c5b2
theterminal <kirilk777@gmail.com>
2022-07-06 08:27:17 +0300

    Refactored code

--- 20220610 - 15 - E - Functions/L15_notes_04_lambda_ex.py
+++ 20220610 - 15 - E - Functions/L15_notes_04_lambda_ex.py
@@ -1,14 +1,18 @@
 # 20220616 - Python Code
 # Lambda examples
 
+
 # ----------- 01 ---------------
+
 
 test_1 = lambda x: x * 10
 
 d = test_1(5)
 print(d)
 
+
 # ----------- 02 ---------------
+
 
 test_2 = lambda x: (x * x) + (3 * x) - 5
 
