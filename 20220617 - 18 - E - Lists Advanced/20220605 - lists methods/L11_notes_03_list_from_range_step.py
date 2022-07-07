110050f43c9ddb4c00ba28dfe1ad57cead5dbe61
theterminal <kirilk777@gmail.com>
2022-07-07 06:43:28 +0300

    Initial commit

---
+++ 20220617 - 18 - E - Lists Advanced/20220605 - lists methods/L11_notes_03_list_from_range_step.py
@@ -0,0 +1,21 @@
+# 20220601 - Python Code - L11 - Lists Basics
+# Notes 03
+
+# ------------------------ Creating a list from range --------
+print('\n----- ex. 1 ----- Creating a list from range -----\n')
+
+
+lst_10 = list(range(1, 10))
+print(lst_10)
+
+print(list(range(1, 10)))
+
+
+# ------------------------ list from range with a step (pos, neg, ...) --------
+print('\n----- ex. 2 ----- List from range with a step (pos, neg, ...) -----\n')
+
+
+lst_20 = list(range(100, 1, -15))
+print(lst_20)
+
+print(list(range(200, -50, -25)))
