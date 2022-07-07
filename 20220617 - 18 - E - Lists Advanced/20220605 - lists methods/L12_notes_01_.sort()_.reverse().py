22696346e7aaf347940ed838374b315182157256
theterminal <kirilk777@gmail.com>
2022-07-07 06:45:03 +0300

    Initial commit

---
+++ 20220617 - 18 - E - Lists Advanced/20220605 - lists methods/L12_notes_01_.sort()_.reverse().py
@@ -0,0 +1,43 @@
+# 20220603 - Python Code - L12 - Lists Basics - Exersice
+# Notes 01
+
+
+# ------------------------ .sort() ---------
+print('\n----- ex. 1 ----- .sort() -----\n')
+
+
+# lst_10.sort()                            # sorts list in ascending order; works for nums and alpha
+# lst_10.sort(reverse=True)                # sorts list in descending order; works for nums and alpha
+# print(lst_10.sort)                       # cannot use this syntax (it returns None). Also, cannot assign it to a variable.
+
+
+lst_10 = [2, 34, -4, 5, 0]
+print(lst_10, '- Original list')
+
+lst_10.sort()
+print(lst_10, '- Same list sorted ascending')
+
+lst_10.sort(reverse=True)
+print(lst_10, '- Same list sorted descending')
+
+print(lst_10.sort(), '             - Cannot use this syntax')
+
+
+# ------------------------ .reverse() --------
+print('\n----- ex. 2 ----- .reverse() -----\n')
+
+
+# list_20.reverse                           # reverses the list
+# list_20[::-1]                             # reverses the list (using slicing)
+# print(list_20.reverse()                   # cannot use this syntax (it returns None). Also, cannot assign it to a variable.
+
+
+lst_20 = [2, 34, -4, 5, 0]
+print(lst_20, '- Original list')
+
+lst_20.reverse()
+print(lst_20, '- Same list reversed')
+
+print(lst_20[::-1], '- Same list reversed again using slicing')
+
+print(lst_20.reverse(), '             - Cannot use this syntax.')
