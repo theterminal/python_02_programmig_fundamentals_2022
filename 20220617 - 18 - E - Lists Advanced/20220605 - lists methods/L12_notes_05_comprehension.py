77ee779f980a6139a282dcf667d32793f215c6fb
theterminal <kirilk777@gmail.com>
2022-07-07 06:52:17 +0300

    Initial commit

---
+++ 20220617 - 18 - E - Lists Advanced/20220605 - lists methods/L12_notes_05_comprehension.py
@@ -0,0 +1,13 @@
+# 20220603 - Python Code - L12 - Lists Basics - Exersice
+# Notes 05
+
+
+# ------------------------ list comprehension -----------------------
+print('\n----- ex. 1 ----- list comprehension --------------------\n')
+
+
+lst_10 = [1, 2, 3, -4, -6]
+print(lst_10, '        - original list')
+
+lst_11 = [i * -1 for i in lst_10]                                                # changed the value of elements using list comprehension
+print(lst_11, '       - changed the value of elements using list comprehension')
