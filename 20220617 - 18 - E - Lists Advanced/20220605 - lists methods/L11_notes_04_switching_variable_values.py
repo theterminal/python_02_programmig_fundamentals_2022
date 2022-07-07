cdb3de83ca57e17e536a23351bdca65c00e24806
theterminal <kirilk777@gmail.com>
2022-07-07 06:44:00 +0300

    Initial commit

---
+++ 20220617 - 18 - E - Lists Advanced/20220605 - lists methods/L11_notes_04_switching_variable_values.py
@@ -0,0 +1,23 @@
+# 20220601 - Python Code - L11 - Lists Basics
+# Notes 04
+
+# ------------------------ Switching variable values --------
+print('\n----- ex. 1 ----- Switching variable values -----\n')
+
+a = 5
+b = 10
+print(f'a = {a}; b = {b}')
+
+a, b = b, a
+print(f'a = {a}; b = {b}')
+
+
+# ------------------------ Switching elements in a list - short way --------
+print('\n----- ex. 2 ----- Switching elements in a list - short way -----\n')
+
+lst_20 = ['a', 'b', 'c']
+print(lst_20)
+
+lst_20[0], lst_20[-1] = lst_20[-1], lst_20[0]
+print(lst_20)
+
