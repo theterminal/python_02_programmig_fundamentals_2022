00e6a929f77cbc01d38b5aaf785020dd96a68087
theterminal <kirilk777@gmail.com>
2022-07-13 12:01:00 +0300

    Initial commit

---
+++ 20220617 - 18 - E - Lists Advanced/20220605 - lists methods/L18_notes_any()_all().py
@@ -0,0 +1,15 @@
+# 20220628 - Python Code
+# any(), all()
+
+
+return_any_01 = any([True, False, False])
+return_any_02 = any([False, False, False])
+
+return_all_01 = all([True, True, True])
+return_all_02 = all([False, True, True])
+
+print(return_any_01)
+print(return_any_02)
+
+print(return_all_01)
+print(return_all_02)
