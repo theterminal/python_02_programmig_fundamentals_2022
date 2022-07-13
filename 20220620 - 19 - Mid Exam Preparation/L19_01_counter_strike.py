cf04ba083feaa7dbde55f8176c099aa671e1e452
theterminal <kirilk777@gmail.com>
2022-07-13 12:04:34 +0300

    Initial commit

---
+++ 20220620 - 19 - Mid Exam Preparation/L19_01_counter_strike.py
@@ -0,0 +1,24 @@
+# 20220621 - Python Code - Mid Exam Preparation
+# 01 - Counter-Strike - judge url: https://judge.softuni.org/Contests/Practice/Index/2305#0
+
+
+energy = int(input())                                               # int [1:10_000]
+battles_won = 0
+
+while True:
+    distance = input()                                              # int [1:10_1000]
+
+    if distance == 'End of battle':
+        print(f'Won battles: {battles_won}. Energy left: {energy}')
+        break
+    else:
+        distance = int(distance)
+
+    if distance <= energy:
+        battles_won += 1
+        energy -= distance
+        if battles_won % 3 == 0:
+            energy += battles_won
+    else:
+        print(f'Not enough energy! Game ends with {battles_won} won battles and {energy} energy')
+        break
