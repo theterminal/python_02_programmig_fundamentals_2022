53fe8485eadd3578b8a2cf72c851f586b8c238e8
theterminal <kirilk777@gmail.com>
2022-07-15 08:53:05 +0300

    Initial commit

---
+++ 20220701 - 22 - E - Objects and Classes/L22_02_weapon.py
@@ -0,0 +1,28 @@
+# 20220701 - Python Code - Objects and Classes - Exercise
+# 02 - Weapon - judge url: https://judge.softuni.org/Contests/Compete/Index/1734#1
+
+
+class Weapon:
+    def __init__(self, bullets: int):
+        self.bullets = bullets
+
+    def shoot(self):
+        if self.bullets > 0:
+            self.bullets -= 1
+            return 'shooting...'
+        return 'no bullets left'
+
+    def __repr__(self):
+        return f'Remaining bullets: {self.bullets}'
+
+
+weapon = Weapon(5)
+
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon)
