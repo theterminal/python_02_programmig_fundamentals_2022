48a00c4e2eec26ed0c7a6d2d64b856d9b389115c
theterminal <kirilk777@gmail.com>
2022-07-15 08:49:22 +0300

    Initial commit

---
+++ 20220629 - 21 - L - Objects and Classes/L21_notes_01_object_class.py
@@ -0,0 +1,21 @@
+# 20220629 - Python Code - Objects and Classes - Lecture
+# Notes 01
+
+
+class Person:
+    def __init__(self, first_name, last_name, age=21):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def say_hello(self):
+        return f'Hello {self.first_name} {self.last_name} {self.age}'
+
+
+person_1 = Person('Ivan', 'Dragan', 32)
+print(person_1.first_name, person_1.age)
+
+person_2 = Person('Kiril', 'Popov')
+print(person_2.last_name, person_2.age)
+
+print(person_1.say_hello())
