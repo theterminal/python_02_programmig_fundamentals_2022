2f42eaa99e83e02f228913e11544d8902abd1c45
theterminal <kirilk777@gmail.com>
2022-07-15 08:49:34 +0300

    Initial commit

---
+++ 20220629 - 21 - L - Objects and Classes/L21_notes_02_dynamically_attaching_attributes.py
@@ -0,0 +1,11 @@
+# 20220629 - Python Code - Objects and Classes - Lecture
+# Notes 02 - Dynamically attaching attributes
+
+
+class Person:
+    pass
+
+
+person_1 = Person()
+person_1.name = 'Ivan Smith'
+print(person_1.name)
