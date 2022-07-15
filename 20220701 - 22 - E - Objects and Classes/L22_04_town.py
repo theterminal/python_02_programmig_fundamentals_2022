6c30dc2ebfecad1ab4f4a78cd46a124c712a02f1
theterminal <kirilk777@gmail.com>
2022-07-15 08:53:37 +0300

    Initial commit

---
+++ 20220701 - 22 - E - Objects and Classes/L22_04_town.py
@@ -0,0 +1,52 @@
+# 20220701 - Python Code - Objects and Classes - Exercise
+# 04 - Town - judge url: https://judge.softuni.org/Contests/Compete/Index/1734#3
+
+
+# ------------------------- version 2 ------------------------------
+
+
+class Town:
+    def __init__(self, name: str, latitude='0°N', longitude='0°E'):
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def set_latitude(self, latitude):
+        self.latitude = latitude
+
+    def set_longitude(self, longitude):
+        self.longitude = longitude
+
+    def __repr__(self):
+        return f'Town: {self.name} | Latitude: {self.latitude} | Longitude: {self.longitude}'
+
+
+town = Town("Sofia")
+town.set_latitude("42° 41\' 51.04\" N")
+town.set_longitude("23° 19\' 26.94\" E")
+print(town)
+
+
+# ------------------------- version 1 ------------------------------
+
+
+class Town:
+    def __init__(self, name: str):
+        self.name = name
+        self.latitude = '0°N'
+        self.longitude = '0°E'
+
+    def set_latitude(self, latitude):
+        self.latitude = latitude
+
+    def set_longitude(self, longitude):
+        self.longitude = longitude
+
+    def __repr__(self):
+        return f'Town: {self.name} | Latitude: {self.latitude} | Longitude: {self.longitude}'
+
+
+town = Town("Sofia")
+town.set_latitude("42° 41\' 51.04\" N")
+town.set_longitude("23° 19\' 26.94\" E")
+print(town)
