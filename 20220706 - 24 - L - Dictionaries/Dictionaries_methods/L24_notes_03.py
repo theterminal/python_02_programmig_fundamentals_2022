938388ecc2936b3f606d196050f29b453c909ce9
theterminal <kirilk777@gmail.com>
2022-07-16 12:34:25 +0300

    Initial commit

---
+++ 20220706 - 24 - L - Dictionaries/Dictionaries_methods/L24_notes_03.py
@@ -0,0 +1,15 @@
+# 20220712 - Python Code - Dictionaries - Lecture
+# Notes - 03 - Dictionaries
+
+
+given_stock = 'cheese 10 bread 5 ham 10 chocolate 3'        # given string separated by space
+dict_from_given_stock = {}                                  # creating an empty dictionary
+given_stock = given_stock.split(' ')                        # list created from the given string
+print(given_stock)
+
+for i in range(0, len(given_stock), 2):                     # loop to fill in the dictionary with 'key-value' pairs
+    key = given_stock[i]
+    value = given_stock[i + 1]
+    dict_from_given_stock[key] = int(value)
+
+print(dict_from_given_stock)
