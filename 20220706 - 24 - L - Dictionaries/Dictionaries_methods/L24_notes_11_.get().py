7cb7250dc3f8dc0fc86e75956502c856245da622
theterminal <kirilk777@gmail.com>
2022-07-19 13:02:15 +0300

    Initial commit

---
+++ 20220706 - 24 - L - Dictionaries/Dictionaries_methods/L24_notes_11_.get().py
@@ -0,0 +1,17 @@
+# 20220712 - Python Code - Dictionaries - Lecture
+# Notes 11
+
+
+# ------------ .get() ----------------------
+
+
+dict_1 = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5
+}
+print(dict_1)
+print(dict_1.get('three'))
+print(dict_1.get("sixteen"))
