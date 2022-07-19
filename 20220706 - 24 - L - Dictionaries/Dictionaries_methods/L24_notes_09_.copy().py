ed843ccd2018b7b6492ea80f77c8f3ab0353a157
theterminal <kirilk777@gmail.com>
2022-07-19 13:01:20 +0300

    Initial commit

---
+++ 20220706 - 24 - L - Dictionaries/Dictionaries_methods/L24_notes_09_.copy().py
@@ -0,0 +1,21 @@
+# 20220712 - Python Code - Dictionaries - Lecture
+# Notes 09
+
+
+# ------------ .copy() ----------------------
+
+
+dict_1 = {
+    'one': 1,
+    'two': 2,
+    'three': 3
+}
+print(dict_1)                               # result: {'one': 1, 'two': 2, 'three': 3}
+
+dict_2 = dict_1.copy()
+print(dict_2)                               # result: {'one': 1, 'two': 2, 'three': 3}
+
+dict_1['one'] = 100
+dict_2['one'] = 1000
+print(dict_1)                               # result: {'one': 100, 'two': 2, 'three': 3}
+print(dict_2)                               # result: {'one': 1000, 'two': 2, 'three': 3}
