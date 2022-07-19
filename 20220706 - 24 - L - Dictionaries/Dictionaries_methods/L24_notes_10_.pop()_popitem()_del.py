3fc55b60cb6c835a976456d7d14a738b0615905a
theterminal <kirilk777@gmail.com>
2022-07-19 13:01:39 +0300

    Initial commit

---
+++ 20220706 - 24 - L - Dictionaries/Dictionaries_methods/L24_notes_10_.pop()_popitem()_del.py
@@ -0,0 +1,35 @@
+# 20220712 - Python Code - Dictionaries - Lecture
+# Notes 10
+
+
+# ------------ .pop() ----------------------
+
+
+dict_1 = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5
+}
+print(dict_1)
+
+print(dict_1.pop('two'))                    # it deletes the pair with the specified key and returns the key
+print(dict_1)
+
+
+# ------------ .popitem() ----------------------
+
+
+print(dict_1.popitem())
+print(dict_1)
+
+
+# ------------ del ----------------------
+
+
+del dict_1['three']
+print(dict_1)
+
+del dict_1
+print(dict_1)                               # it returns 'undefined' because del deletes the dictionary
