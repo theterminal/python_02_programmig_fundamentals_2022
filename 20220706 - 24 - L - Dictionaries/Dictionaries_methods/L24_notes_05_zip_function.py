a403af46a7c8df4ef5a157df12cf5ad2eb65190e
theterminal <kirilk777@gmail.com>
2022-07-19 12:46:05 +0300

    Initial commit

---
+++ 20220706 - 24 - L - Dictionaries/Dictionaries_methods/L24_notes_05_zip_function.py
@@ -0,0 +1,9 @@
+# 20220712 - Python Code - Dictionaries - Lecture
+# Notes - 05 - Dictionaries - zip function
+
+
+questions = ['name', 'quest', 'favorite color']
+answers = ['lancelot', 'the holy grail', 'blue']
+
+for q, a in zip(questions, answers):
+    print(f'What is your {q}?  It is {a}.')
