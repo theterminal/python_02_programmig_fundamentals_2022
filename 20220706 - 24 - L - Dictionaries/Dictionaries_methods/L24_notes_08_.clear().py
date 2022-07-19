657524a644fb527a23a8cb1230561197cdeda6a3
theterminal <kirilk777@gmail.com>
2022-07-19 12:57:46 +0300

    Initial commit

---
+++ 20220706 - 24 - L - Dictionaries/Dictionaries_methods/L24_notes_08_.clear().py
@@ -0,0 +1,18 @@
+# 20220712 - Python Code - Dictionaries - Lecture
+# Notes 08
+
+
+# ------------ .clear() ----------------------
+
+
+test_dict = {
+    'one': 1,
+    'two': 2,
+    'three': 3
+}
+
+print(test_dict)
+
+test_dict.clear()
+
+print(test_dict)
