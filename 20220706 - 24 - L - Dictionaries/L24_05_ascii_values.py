4a98e67969c9e727d973b4c0431b11bd8dc46f3d
theterminal <kirilk777@gmail.com>
2022-07-20 13:33:40 +0300

    Initial commit

---
+++ 20220706 - 24 - L - Dictionaries/L24_05_ascii_values.py
@@ -0,0 +1,21 @@
+# 20220712 - Python Code - Dictionaries - Lecture
+# 05 - ASCII Values - judge url: https://judge.softuni.org/Contests/Practice/Index/1736#4
+
+
+# ------------------------------- version 1 ------------------ judge: 100%
+
+
+data_in = input().split(', ')
+
+dict_data_in = {}
+
+for i in data_in:
+    dict_data_in[i] = ord(i)
+
+print(dict_data_in)
+
+
+# ------------------------------- version 2 ------------------ judge: 100%
+
+
+print({i: ord(i) for i in input().split(', ')})
