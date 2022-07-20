b874573725a6a5ca3ee1713e55ac3a189295010e
theterminal <kirilk777@gmail.com>
2022-07-20 13:32:44 +0300

    Initial commit

---
+++ 20220706 - 24 - L - Dictionaries/L24_04_students.py
@@ -0,0 +1,26 @@
+# 20220706 - Python Code - Dictionaries - Lecture
+# 04 - Students - judge url: https://judge.softuni.org/Contests/Practice/Index/1736#3
+
+
+students = {}
+
+while True:
+    command = input()
+    if command[0].islower():
+        break
+
+    tokens = command.split(':')
+
+    student_name = tokens[0]
+    student_id = tokens[1]
+    student_course = tokens[2]
+
+    students[student_id] = {student_name: student_course}
+
+if '_' in command:
+    command = command.replace('_', ' ')
+
+for key, value in students.items():
+    for key_2, value_2 in value.items():
+        if command in value_2:
+            print(key_2, '-', key)
