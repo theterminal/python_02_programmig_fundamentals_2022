e4aae184f3154322b14b5efcca70f209a4baaf9f
theterminal <kirilk777@gmail.com>
2022-07-21 12:51:16 +0300

    Initial commit

---
+++ 20220708 - 25 - E - Dictionaries/L25_06_orders.py
@@ -0,0 +1,25 @@
+# 20220714 - Python Code - Dictionaries - Exercise
+# 06 - Orders - judge url: https://judge.softuni.org/Contests/Compete/Index/1737#5
+
+
+# ---------------------- version 1 ------------------- judge: 100%
+
+
+goods = {}
+
+while True:
+    data_in = input()
+    if data_in == 'buy':
+        break
+
+    name, price, quantity = data_in.split(' ')
+
+    if name not in goods:
+        goods[name] = {0.00: 0}
+
+    for k, v in goods[name].items():
+        goods[name] = {float(price): v + int(quantity)}
+
+for k_1, v_1 in goods.items():
+    for k_2, v_2 in v_1.items():
+        print(f'{k_1} -> {(k_2 * v_2):.2f}')
