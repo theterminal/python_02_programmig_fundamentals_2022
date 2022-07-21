f20b90705d338262613d2463b3245bc28dc7e762
theterminal <kirilk777@gmail.com>
2022-07-21 12:49:42 +0300

    Initial commit

---
+++ 20220708 - 25 - E - Dictionaries/L25_04_phonebook.py
@@ -0,0 +1,25 @@
+# 20220713 - Python Code - Dictionaries - Exercise
+# 04 - Phonebook - judge url: https://judge.softuni.org/Contests/Compete/Index/1737#3
+
+
+# ------------- version 1 -------------------- judge: 100%
+
+
+dct = {}
+
+while True:
+    data = input()
+    if data.isdigit():
+        data = int(data)
+        break
+
+    name, number = data.split('-')
+
+    dct[name] = number
+
+for i in range(data):
+    person = input()
+    if person in dct.keys():
+        print(f'{person} -> {dct[person]}')
+    else:
+        print(f'Contact {person} does not exist.')
