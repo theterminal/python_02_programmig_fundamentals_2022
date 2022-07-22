6e089682361c1bfde93e9d1bfba539353455fe60
theterminal <kirilk777@gmail.com>
2022-07-23 00:42:44 +0300

    Initial commit

---
+++ 20220713 - 27 - L - Text Processing/L27_05_digits_letters_and_other.py
@@ -0,0 +1,43 @@
+# 20220715 - Python Code - String Processing - Lecture
+# 05 - Digits, Letters and Other - judge url: https://judge.softuni.org/Contests/Practice/Index/1739#4
+
+
+# ----------------- version 2 ------------------- judge: 100%
+
+
+def get_digits(data):
+    return ''.join([str(ch) for ch in data if ch.isdigit()])
+
+
+def get_letters(data):
+    return ''.join([ch for ch in data if ch.isalpha()])
+
+
+def get_other(data):
+    return ''.join([ch for ch in data if not ch.isdigit() and not ch.isalpha()])
+
+
+data_in = input()
+print(get_digits(data_in))
+print(get_letters(data_in))
+print(get_other(data_in))
+
+
+# ----------------- version 1 ------------------- judge: 100%
+
+
+data_in: str = input()
+
+digits = alpha = other = ''
+
+for symbol in data_in:
+    if symbol.isdigit():
+        digits += symbol
+    elif symbol.isalpha():
+        alpha += symbol
+    else:
+        other += symbol
+
+print(digits)
+print(alpha)
+print(other)
