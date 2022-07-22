e27eff8dba64817569e94d702930ec7db87f603c
theterminal <kirilk777@gmail.com>
2022-07-23 00:43:06 +0300

    Initial commit

---
+++ 20220713 - 27 - L - Text Processing/L27_notes_01_str()_.split().py
@@ -0,0 +1,22 @@
+# 20220714 - Python Code - String Processing - Lecture
+# 01 - Notes
+
+
+# ---------------- str() ---------------------------
+
+
+x = 3.5
+print(type(x))
+
+str_x = str(x)
+print(type(str_x))
+
+
+# ---------------- split() -------------------------
+
+
+txt = 'Hello, my name is, Peter'
+print(txt)
+
+lst = txt.split(', ')
+print(lst)
