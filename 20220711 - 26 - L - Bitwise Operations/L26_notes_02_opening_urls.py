e9aaf104f0c174dcf93f90d05915aabef3da5d5d
theterminal <kirilk777@gmail.com>
2022-07-22 19:46:58 +0300

    Initial commit

---
+++ 20220711 - 26 - L - Bitwise Operations/L26_notes_02_opening_urls.py
@@ -0,0 +1,14 @@
+# 20220713 - Python Code - from: https://github.com/CodingPawn/python/blob/main/web-automation/web_automation.py
+
+
+import webbrowser
+
+
+def web_automation():
+    urls = ("stackoverflow.com", "github.com", "neuralnine.com")        # URLs we're opening
+
+    for url in urls:
+        webbrowser.open("https://" + url)       # Not using a specific Chrome directory. Add 'https://' to the URL to open in default browser.
+
+
+web_automation()
