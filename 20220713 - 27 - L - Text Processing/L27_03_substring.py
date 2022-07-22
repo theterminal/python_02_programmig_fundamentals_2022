2d943efc83a64652ac1906aba31146b50fceb36e
theterminal <kirilk777@gmail.com>
2022-07-22 19:48:59 +0300

    Initial commit

---
+++ 20220713 - 27 - L - Text Processing/L27_03_substring.py
@@ -0,0 +1,14 @@
+# 20220714 - Python Code - String Processing - Lecture
+# 03 - Substring - judge url: https://judge.softuni.org/Contests/Practice/Index/1739#2
+
+
+# ------------------------- version 1 -------------- judge: 100%
+
+
+first: str = input()
+second: str = input()
+
+while first in second:
+    second = second.replace(first, '')
+
+print(second)
