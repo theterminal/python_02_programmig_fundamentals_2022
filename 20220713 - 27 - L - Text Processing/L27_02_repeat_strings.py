da31d81d720ff2e200c0a4842f6ba4b3356cd94c
theterminal <kirilk777@gmail.com>
2022-07-22 19:48:32 +0300

    Initial commit

---
+++ 20220713 - 27 - L - Text Processing/L27_02_repeat_strings.py
@@ -0,0 +1,31 @@
+# 20220714 - Python Code - String Processing - Lecture
+# 02 - Repeat Strings - judge url: https://judge.softuni.org/Contests/Practice/Index/1739#1
+
+
+# --------- version 3 -------------- judge: 100%
+
+
+def repeat_word(word):
+    return word * len(word)
+
+
+words: list = input().split(' ')
+print(''.join(map(repeat_word, words)))
+
+
+# --------- version 2 -------------- judge: 100%
+
+
+print(''.join([i * len(i) for i in input().split(' ')]))
+
+
+# --------- version 1 -------------- judge: 100%
+
+
+str_in = input().split(' ')
+str_out = ''
+
+for s in str_in:
+    str_out += (s * len(s))
+
+print(str_out)
