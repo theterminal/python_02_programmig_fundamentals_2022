851f067348cc3477bb35d9e2df0c24284d8192bc
theterminal <kirilk777@gmail.com>
2022-07-22 19:47:59 +0300

    Initial commit

---
+++ 20220713 - 27 - L - Text Processing/L27_01_reverse_strings.py
@@ -0,0 +1,40 @@
+# 20220714 - Python Code - String Processing - Lecture
+# 01 - Reverse Strings - judge url: https://judge.softuni.org/Contests/Practice/Index/1739#0
+
+
+# ------------------- version 1 ------------ slicing ---------- judge: 100%
+
+
+while True:
+    data_in = input()
+    if data_in == 'end':
+        break
+
+    print(f'{data_in} = {data_in[::-1]}')
+
+
+# ------------------- version 2 ------------ reversed ---------- judge: 100%
+
+rev_d = ''
+
+while True:
+    data_in = input()
+    if data_in == 'end':
+        break
+
+    for i in reversed(data_in):
+        rev_d += i
+
+    print(data_in, '=', rev_d)
+    rev_d = ''
+
+
+# ------------------- version 3 ------------ reversed ---------- judge: 100%
+
+
+while True:
+    data_in = input()
+    if data_in == 'end':
+        break
+
+    print(f'{data_in} = {"".join(reversed(data_in))}')
