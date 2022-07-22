19ebf5485d78872007de577aa062e8f56e0cff02
theterminal <kirilk777@gmail.com>
2022-07-22 19:42:50 +0300

    Initial commit

---
+++ 20220708 - 25 - E - Dictionaries/L25_10_company_users.py
@@ -0,0 +1,49 @@
+# 20220714 - Python Code - Dictionaries - Exercise
+# 10 - Company Users - judge url: https://judge.softuni.org/Contests/Compete/Index/1737#9
+
+
+# ------------------------ version 2 ----------------- judge: 100%
+
+db = {}
+
+while True:
+    data_in = input()
+    if data_in == 'End':
+        break
+
+    company, employee_id = data_in.split(' -> ')
+
+    if company not in db:
+        db[company] = [employee_id]
+    else:
+        if employee_id not in db[company]:
+            db[company] += [employee_id]
+
+for k, v in db.items():
+    print(f'{k}')
+    for e_id in v:
+        print(f'-- {e_id}')
+
+
+# ------------------------ version 1 ----------------- judge: 100%
+
+
+db = {}
+
+while True:
+    data_in = input()
+
+    if data_in == 'End':
+        for k, v in db.items():
+            print(f'{k}')
+            for e_id in v:
+                print(f'-- {e_id}')
+        break
+
+    company, employee_id = data_in.split(' -> ')
+
+    if company not in db:
+        db[company] = [employee_id]
+    else:
+        if employee_id not in db[company]:
+            db[company] += [employee_id]
