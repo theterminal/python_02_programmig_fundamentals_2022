e692ddeb42e318ccf6ab538406f6ec5e7d2165a3
theterminal <kirilk777@gmail.com>
2022-07-23 00:41:25 +0300

    Initial commit

---
+++ 20220713 - 27 - L - Text Processing/L27_04_text_filter.py
@@ -0,0 +1,28 @@
+# 20220715 - Python Code - String Processing - Lecture
+# 04 - Text Filter - judge url: https://judge.softuni.org/Contests/Practice/Index/1739#3
+
+
+# ------------------ version 2 ----------------- judge: 100%
+
+
+banned_words = input().split(', ')
+data_in = input()
+
+for word in banned_words:
+    if word in data_in:
+        data_in = data_in.replace(word, '*' * len(word))
+
+print(data_in)
+
+
+# ------------------ version 1 ----------------- judge: 100%
+
+
+banned_words: list = input().split(', ')
+text: str = input()
+
+for word in banned_words:
+    while word in text:
+        text = text.replace(word, '*' * len(word))
+
+print(text)
