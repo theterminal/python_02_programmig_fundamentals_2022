a0fef4ae62f63809d36b14eac0ee6955dd796917
theterminal <kirilk777@gmail.com>
2022-07-23 00:44:10 +0300

    Initial commit

---
+++ 20220713 - 27 - L - Text Processing/L27_notes_04_.strip()_.lstrip()_.rstrip().py
@@ -0,0 +1,32 @@
+# 20220715 - Python Code - String Processing - Lecture
+# 04 - Notes
+
+
+# ---------------- .strip() -------------------------
+
+
+txt = ' This is a string with white space on the left and on the right. '
+print(txt)
+
+txt = txt.strip()
+print(txt)
+
+print()
+# ---------------- .lstrip() -------------------------
+
+
+txt = ' This is a string with white space on the left and on the right. '
+print(txt)
+
+txt = txt.lstrip()
+print(txt)
+
+print()
+# ---------------- .rstrip() -------------------------
+
+
+txt = ' This is a string with white space on the left and on the right. '
+print(txt)
+
+txt = txt.rstrip()
+print(txt)
