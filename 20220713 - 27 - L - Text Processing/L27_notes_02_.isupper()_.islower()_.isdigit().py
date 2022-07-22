51e0656d2d24fd5bf84a68cf937c6de7c7772711
theterminal <kirilk777@gmail.com>
2022-07-23 00:43:23 +0300

    Initial commit

---
+++ 20220713 - 27 - L - Text Processing/L27_notes_02_.isupper()_.islower()_.isdigit().py
@@ -0,0 +1,23 @@
+# 20220714 - Python Code - String Processing - Lecture
+# 02 - Notes
+
+
+# ---------------- .isdigit() -------------------------
+
+
+print('1'.isdigit())
+print('p'.isdigit())
+
+print()
+# ---------------- .isupper() -------------------------
+
+
+print('p'.isupper())
+print('K'.isupper())
+
+print()
+# ---------------- .islower() -------------------------
+
+
+print('p'.islower())
+print('K'.islower())
