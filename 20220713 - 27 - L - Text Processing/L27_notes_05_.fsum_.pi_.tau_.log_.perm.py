a9e9018dfdc6ec85377137a0d537a4074b4d9bfa
theterminal <kirilk777@gmail.com>
2022-07-23 00:44:42 +0300

    Initial commit

---
+++ 20220713 - 27 - L - Text Processing/L27_notes_05_.fsum_.pi_.tau_.log_.perm.py
@@ -0,0 +1,30 @@
+# 20220715 - Python Code - String Processing - Lecture
+# 05 - Notes
+
+
+# from: https://docs.python.org/3.10/library/math.html#module-math
+import math
+
+
+print(sum([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, ]))
+print(math.fsum([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, ]))
+
+
+print()
+# -------------- permutations, variations --------------------
+
+
+print(math.perm(7))                     # number of permutation from 7 elements
+print(math.perm(7, 3))                  # number of variations from 7 elements in groups of 3
+
+
+print()
+
+
+print(math.log(4, 2))
+print()
+
+print(math.pi)
+print(math.tau)
+
+
