7b100f07409fc25939983877f964d52311ed1fec
theterminal <kirilk777@gmail.com>
2022-07-25 00:04:41 +0300

    Initial commit

---
+++ 20220715 - 28 - E - Text Processing/L28_06_replace_repeating_chars.py
@@ -0,0 +1,42 @@
+# 20220715 - Python Code - String Processing - Exercise
+# 06 - Replace Repeating Chars - judge url: https://judge.softuni.org/Contests/Compete/Index/1740#5
+
+
+# ------------------------ version 1 -------------------- judge: 100%
+
+
+data_in = input()
+chars_to_print = ''
+last_char = ''
+
+for i in range(len(data_in)):
+    if i == 0:
+        chars_to_print = data_in[i]
+        last_char = data_in[i]
+        continue
+
+    if data_in[i] == last_char:
+        continue
+    else:
+        chars_to_print += data_in[i]
+        last_char = data_in[i]
+
+print(chars_to_print)
+
+
+""" Replace Repeating Chars
+
+Write a program that reads a string from the console and replaces
+any sequence of the same letters with a single corresponding letter.
+
+
+
+Examples:
+
+Input 1:	                        Output 1:
+aaaaabbbbbcdddeeeedssaa	            abcdedsa
+
+Input 2:	                        Output 2:
+qqqwerqwecccwd	                    qwerqwecwd
+
+"""
