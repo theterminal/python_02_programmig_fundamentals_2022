0efe615353feca174dd7b87301755b30cb0a0899
theterminal <kirilk777@gmail.com>
2022-07-25 00:03:31 +0300

    Initial commit

---
+++ 20220715 - 28 - E - Text Processing/L28_04_caesar_cipher.py
@@ -0,0 +1,14 @@
+# 20220715 - Python Code - String Processing - Exercise
+# 04 - Caesar Cipher - judge url: https://judge.softuni.org/Contests/Compete/Index/1740#3
+
+
+# ------------------------ version 1 -------------------- judge: 100%
+
+
+str_in = input()
+str_out = ''
+
+for char in str_in:
+    str_out += chr(ord(char) + 3)
+
+print(str_out)
