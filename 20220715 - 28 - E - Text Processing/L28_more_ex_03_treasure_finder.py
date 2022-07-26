4a663d8e5dc4f4d56fd940ea838130804051bc83
theterminal <kirilk777@gmail.com>
2022-07-26 10:09:27 +0300

    Initial commit

---
+++ 20220715 - 28 - E - Text Processing/L28_more_ex_03_treasure_finder.py
@@ -0,0 +1,16 @@
+# 20220721 - Python Code - String Processing - Exercise
+# More Exercise 03 - Treasure Finder - judge url: https://judge.softuni.org/Contests/Practice/Index/1741#2
+
+
+# ---------------------- version 1 ----------------------------- judge: %
+
+
+key = input()
+
+while True:
+    command = input()
+    if command == 'find':
+        break
+    print(command)
+
+print(key)
