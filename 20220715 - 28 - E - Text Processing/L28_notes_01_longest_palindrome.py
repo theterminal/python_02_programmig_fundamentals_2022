f86f0a94ef79813fb8a9a916b6ac44ffb34c0386
theterminal <kirilk777@gmail.com>
2022-07-26 10:11:41 +0300

    Initial commit

---
+++ 20220715 - 28 - E - Text Processing/L28_notes_01_longest_palindrome.py
@@ -0,0 +1,23 @@
+# 20220719 - Python Code - Lecture - Problem Solving
+# 01 - The Longest Palindrome in a String
+
+
+txt = 'abbabbaazzaab'
+longest_palindrome = ''
+
+for i in range(len(txt), 1, -1):
+    for j in range(0, i):
+        if txt[j: i] == ''.join(reversed(txt[j: i])) and len(txt[j: i]) > 1:
+            if len(txt[j: i]) > len(longest_palindrome):
+                longest_palindrome = txt[j: i]
+
+print(f'The longest palindrome is: \'{longest_palindrome}\' and it contains {len(longest_palindrome)} symbols.')
+
+
+""" ---------------- Find the longest palindrome in a string of symbols ----------------
+
+
+Find the longest palindrome in a string of symbols
+
+
+"""
