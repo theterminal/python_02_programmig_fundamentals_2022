e3e2b33546437dcaff1d3beeec8f029057e40048
theterminal <kirilk777@gmail.com>
2022-07-29 00:06:51 +0300

    Added full problem description

---
+++ 20220518 - 05 - L - Basic Syntax, Conditional Statements, Loops/L05_03_word_reverse.py
@@ -0,0 +1,43 @@
+# 20220518 - Python - Python Fundamentals - L5 - Basic Syntax, Conditional Statements, Loops
+# 03 - Word Reverse - judge url: https://judge.softuni.org/Contests/Practice/Index/1718#2
+
+
+# --------------------- version 2 --------------------------- judge: 100%
+
+
+word_2 = input()
+print(word_2[::-1])
+
+
+# --------------------- version 1 --------------------------- judge: 100%
+
+
+word_1 = input()
+reversed_word = ''
+
+for i in range(len(word_1) - 1, -1, -1):
+    reversed_word += word_1[i]
+
+print(reversed_word)
+
+
+""" --------- Word Reverse ---------
+
+
+Write a program that receives a single word, reverses it, and prints it.
+
+
+------------- Examples -------------
+
+
+Input 1:                Output 1:
+-------                 --------
+Python                  nohtyP
+
+
+Input 2:                Output 2:
+-------                 --------
+banana                  ananab
+
+
+"""
