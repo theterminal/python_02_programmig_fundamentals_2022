c7ff733add9d5a49a15a928bf632029a76026a7f
theterminal <kirilk777@gmail.com>
2022-07-30 00:08:46 +0300

    Fixed some formatting

--- 20220520 - 06 - E - Basic Syntax, Conditional Statement, Loops/L06_06_string_pureness.py
+++ 20220520 - 06 - E - Basic Syntax, Conditional Statement, Loops/L06_06_string_pureness.py
@@ -2,7 +2,7 @@
 # 06 - String Pureness - judge url: https://judge.softuni.org/Contests/Compete/Index/1719#5
 
 
-# ----------------- version 4 --------------------
+# ----------------- version 4 -------------------- judge: 100%
 
 
 num_n = int(input())
@@ -19,7 +19,7 @@ for _ in range(num_n):
         print(f'{str_to_test} is pure.')
 
 
-# ----------------- version 3 --------------------
+# ----------------- version 3 -------------------- judge: 100%
 
 
 num_n = int(input())
@@ -33,7 +33,7 @@ for _ in range(num_n):
         print(f'{str_to_test} is pure.')
 
 
-# ----------------- version 2 --------------------
+# ----------------- version 2 -------------------- judge: 100%
 
 
 num_n = int(input())
@@ -48,7 +48,7 @@ for i in range(num_n):
         print(f'{str_to_test} is pure.')
 
 
-# ------------------ version 1 -------------------
+# ------------------ version 1 ------------------- judge: 100%
 
 
 num_n = int(input())
@@ -65,3 +65,56 @@ for i in range(num_n):
         print(f'{str_to_test} is pure.')
 
     flag = False
+
+
+''' ------------- String Pureness ----------------
+
+
+You will be given number 'n'.
+After that, you'll receive different strings 'n' times.
+
+Your task is to check if the given strings are pure, meaning that they do NOT consist of any of the characters:
+
+    comma ","
+    period "."
+    underscore "_"
+    
+    •	If a string is pure, print "{string} is pure."
+    •	Otherwise, print "{string} is not pure!"
+
+
+--------- Test Data ----------
+
+
+Input 1:
+-------
+2
+pure string
+not_pure_string
+
+
+Output 1:
+--------
+pure string is pure.
+not_pure_string is not pure!
+
+
+------------------------------
+
+
+Input 2:
+-------
+3
+SoftUni
+12345
+string.pureness
+
+
+Output 2:
+--------
+SoftUni is pure.
+12345 is pure.
+string.pureness is not pure!
+
+
+'''
