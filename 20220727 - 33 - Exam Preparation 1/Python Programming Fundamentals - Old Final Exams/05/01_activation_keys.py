e822ab8872255eab0a1996fa8b51c120581adc93
theterminal <kirilk777@gmail.com>
2022-08-04 00:17:37 +0300

    Initial commit

---
+++ 20220727 - 33 - Exam Preparation 1/Python Programming Fundamentals - Old Final Exams/05/01_activation_keys.py
@@ -0,0 +1,129 @@
+# 20220729 - Python Code - Exam Preparation 2
+# 01 - Activation Keys - judge url: https://judge.softuni.org/Contests/Practice/Index/2302#0
+
+
+# ------------------------- version 1 ------------------------ judge: 100%
+
+
+key = input()
+
+while True:
+    data_in = input()
+    if data_in == 'Generate':
+        break
+
+    command = data_in.split('>>>')
+
+    if command[0] == 'Contains':
+
+        if command[1] in key:
+            print(f'{key} contains {command[1]}')
+        else:
+            print('Substring not found!')
+
+    elif command[0] == 'Flip':
+
+        if command[1] == 'Upper':
+            key = key[:int(command[2])] + key[int(command[2]): int(command[3])].upper() + key[int(command[3]):]
+            print(key)
+
+        elif command[1] == 'Lower':
+            key = key[:int(command[2])] + key[int(command[2]): int(command[3])].lower() + key[int(command[3]):]
+            print(key)
+
+    elif command[0] == 'Slice':
+        key = key[: int(command[1])] + key[int(command[2]):]
+        print(key)
+
+print(f'Your activation key is: {key}')
+
+
+""" ---------------- Activation Keys ------------------
+
+
+You are about to make some good money, but first, you need to think of a way to verify who paid for your product and who didn't.
+You have decided to let people use the software for a free trial period and then require an activation key to continue using the product.
+Before you can cash out, the last step is to design a program that creates unique activation keys for each user.
+So, waste no more time and start typing!
+
+The first line of the input will be your raw activation key.
+It will consist of letters and numbers only.
+After that, until the "Generate" command is given, you will be receiving strings with instructions for different operations that need to be performed upon the raw activation key.
+
+There are several types of instructions, split by ">>>":
+
+    •	"Contains>>>{substring}":
+        o	If the raw activation key contains the given substring, prints: "{raw activation key} contains {substring}".
+        o	Otherwise, prints: "Substring not found!"
+
+    •	"Flip>>>Upper/Lower>>>{startIndex}>>>{endIndex}":
+        o	Changes the substring between the given indices (the end index is exclusive) to upper or lower case and then prints the activation key.
+        o	All given indexes will be valid.
+
+    •	"Slice>>>{startIndex}>>>{endIndex}":
+        o	Deletes the characters between the start and end indices (the end index is exclusive) and prints the activation key.
+        o	Both indices will be valid.
+
+
+Input
+-----
+    •	The first line of the input will be a string consisting of letters and numbers only.
+    •	After the first line, until the "Generate" command is given, you will be receiving strings.
+
+Output
+------
+    •	After the "Generate" command is received, print:
+        o	"Your activation key is: {activation key}"
+
+
+-------------------- Test Data ---------------------
+
+
+Input 1:
+-------
+abcdefghijklmnopqrstuvwxyz
+Slice>>>2>>>6
+Flip>>>Upper>>>3>>>14
+Flip>>>Lower>>>5>>>7
+Contains>>>def
+Contains>>>deF
+Generate
+
+
+Output 1:
+--------
+abghijklmnopqrstuvwxyz
+abgHIJKLMNOPQRstuvwxyz
+abgHIjkLMNOPQRstuvwxyz
+Substring not found!
+Substring not found!
+Your activation key is: abgHIjkLMNOPQRstuvwxyz
+
+
+-------------------------------------------------------
+
+
+Input 2:
+-------
+134softsf5ftuni2020rockz42
+Slice>>>3>>>7
+Contains>>>-rock
+Contains>>>-uni-
+Contains>>>-rocks
+Flip>>>Upper>>>2>>>8
+Flip>>>Lower>>>5>>>11
+Generate
+
+
+Output 2:
+--------
+134sf5ftuni2020rockz42
+Substring not found!
+Substring not found!
+Substring not found!
+134SF5FTuni2020rockz42
+134SF5ftuni2020rockz42
+Your activation key is: 134SF5ftuni2020rockz42
+
+
+"""
