875ef3e20fa893de8a9a44f0fc7dc0c41b60e3ff
theterminal <kirilk777@gmail.com>
2022-08-07 00:23:06 +0300

    Refactoring

--- 20220525 - 08 - L - Data Types and Variables/L08_notes_02_bool_statements.py
+++ 20220525 - 08 - L - Data Types and Variables/L08_notes_02_bool_statements.py
@@ -1,24 +1,27 @@
-# 20220529 - Python - Python Fundamentals - L8 - Data Types and Variables
+# 20220529 - Python - Python Fundamentals - L08 - Data Types and Variables
 # notes 02 - Bool
 
 
-# The following values will return False when bool tested!
-# 0, -0, False, '',None
+# The values (0, -0, False, '',None) will return 'False' when 'bool' tested!
+
 
 print('\n------- Example 1 ----------\n')
 
 
 number = 1
+
 if number:
     print(f'It works! It is a number and the number is: {number}')
 else:
     print('else')
 
 
+# -----------------------------------------------------------------
 print('\n-------- Example 2 ----------\n')
 
 
 number = None
+
 if number:
     print(f'It is a number! And the number is: {number}')
 else:
