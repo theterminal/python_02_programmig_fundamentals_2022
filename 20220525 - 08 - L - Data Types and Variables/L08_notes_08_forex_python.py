e2e92fd0609cdda31b1635fdddd680a7aad03e67
theterminal <kirilk777@gmail.com>
2022-08-07 00:26:08 +0300

    Refactoring

--- 20220525 - 08 - L - Data Types and Variables/L08_notes_08_forex_python.py
+++ 20220525 - 08 - L - Data Types and Variables/L08_notes_08_forex_python.py
@@ -6,14 +6,18 @@ from forex_python.converter import CurrencyRates
 # must have module 'forex-python' installed via terminal command 'pip install forex-python'
 
 
-amount = float(input('Enter GBP amount: '))
-c = CurrencyRates()
-current_rate = c.get_rate('GBP', 'USD')
-result = amount * current_rate
-print(f'GBP {amount} = USD {result:.3f}')
+# amount = float(input('Enter GBP amount: '))
+# c = CurrencyRates()
+# current_rate = c.get_rate('GBP', 'USD')
+# result = amount * current_rate
+# print(f'GBP {amount} = USD {result:.3f}')
 
+
+print(f"GBP / USD = {CurrencyRates().get_rate('GBP', 'USD'):.4f}")
 print(f"EUR / USD = {CurrencyRates().get_rate('EUR', 'USD')}")
 print(f"GBP / USD = {(CurrencyRates().get_rate('GBP', 'USD')):.4f}")
 print(f"EUR / BGN = {CurrencyRates().get_rate('EUR', 'BGN')}")
 print(f"USD / BGN = {(CurrencyRates().get_rate('USD', 'BGN')):.4f}")
-
+print(f"GBP / BGN = {(CurrencyRates().get_rate('GBP', 'BGN')):.4f}")
+print(f"RUB / BGN = {(CurrencyRates().get_rate('RUB', 'BGN')):.4f}")
+print(f"BGN / RUB = {1 / (CurrencyRates().get_rate('RUB', 'BGN')):.4f}")
