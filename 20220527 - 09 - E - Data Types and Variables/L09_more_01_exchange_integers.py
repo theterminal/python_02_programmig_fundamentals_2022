7b6606d4650cf12623bb69401408e80db7c8fad8
theterminal <kirilk777@gmail.com>
2022-08-11 00:04:51 +0300

    Added problem description

--- 20220527 - 09 - E - Data Types and Variables/L09_more_01_exchange_integers.py
+++ 20220527 - 09 - E - Data Types and Variables/L09_more_01_exchange_integers.py
@@ -2,7 +2,7 @@
 # More Exercises 01 - Exchange Integers - judge url: https://judge.softuni.org/Contests/Practice/Index/1723#0
 
 
-# ---------------- version 2 ------------------
+# ---------------- version 2 ------------------ judge 100%
 
 
 num_1, num_2 = int(input()), int(input())
@@ -23,3 +23,53 @@ num_1, num_2 = int(input()), int(input())
 
 print(f'Before:\na = {num_1}\nb = {num_2}')
 print(f'After:\na = {num_2}\nb = {num_1}')
+
+
+""" ------------------- Exchange Integers ----------------------
+
+
+Read two integer numbers and, after that, exchange their values.
+Print the variable values before and after the exchange, as shown below:
+
+
+-------- Test Data ---------
+
+
+Input 1:
+-------
+5
+10
+
+
+Output 1:
+--------
+Before:
+a = 5
+b = 10
+After:
+a = 10
+b = 5
+
+
+----------------------------
+
+
+Input 2:
+-------
+10
+20
+
+
+Output 2:
+--------
+Before:
+a = 10
+b = 20
+After:
+a = 20
+b = 10
+
+
+---------------------------
+
+"""
