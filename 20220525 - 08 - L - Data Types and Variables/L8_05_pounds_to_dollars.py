871d70c1aa1f545b3e351da53e33db319dee7810
theterminal <kirilk777@gmail.com>
2022-08-10 16:43:38 +0300

    File deleted

--- 20220525 - 08 - L - Data Types and Variables/L8_05_pounds_to_dollars.py
+++
@@ -1,9 +0,0 @@
-# 20220529 - Python - Python Fundamentals - L9 - Data Types and Variables
-# 05 - Pounds to Dollars - judge url: https://judge.softuni.org/Contests/Practice/Index/1721#4
-
-
-gbp_entered_to_convert_to_usd = int(input())
-gbp_to_usd_rate = 1.31
-
-usd = gbp_entered_to_convert_to_usd * gbp_to_usd_rate
-print(f'{usd:.3f}')
