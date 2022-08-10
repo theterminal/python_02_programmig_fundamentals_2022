908f54ba554655cc3ed4a38f6ef285bf03b12364
theterminal <kirilk777@gmail.com>
2022-08-10 16:45:02 +0300

    Refactoring

---
+++ 20220527 - 09 - E - Data Types and Variables/L09_02_chars_to_string.py
@@ -0,0 +1,69 @@
+# 20220527 - Python - Python Fundamentals - L9 -  - Data Types and Variables
+# 02 - Chars to String - judge url: https://judge.softuni.org/Contests/Compete/Index/1722#1
+
+
+# ---------------------------- version 1 ------------------------ judge: 100%
+
+
+str_1 = input()
+str_2 = input()
+str_3 = input()
+
+str_result = str_1 + str_2 + str_3
+
+print(str_result)
+
+
+""" ---------------------- Chars To String ---------------------------------
+
+
+Write a program that receives 3 characters.
+Concatenate all the characters into one string and print it on the console.
+
+
+------------ Test Data ------------
+
+
+Input 1:
+-------
+a
+b
+c
+
+
+Output 1:
+--------
+abc
+
+
+------------------------------------
+
+
+Input 2:
+-------
+%
+2
+o
+
+
+Output 2:
+--------
+%2o
+
+
+------------------------------------
+
+
+Input 3:
+-------
+1
+5
+p
+
+
+Output 3:
+--------
+15p
+
+
+"""
