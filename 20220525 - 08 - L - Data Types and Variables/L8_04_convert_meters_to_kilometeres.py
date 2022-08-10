ac8a0fc38c9eb7f5f7e9ce9b1348f3cccc02e6a6
theterminal <kirilk777@gmail.com>
2022-08-10 16:43:30 +0300

    File deleted

--- 20220525 - 08 - L - Data Types and Variables/L8_04_convert_meters_to_kilometeres.py
+++
@@ -1,9 +0,0 @@
-# 20220529 - Python - Python Fundamentals - L9 - Data Types and Variables
-# 04 - Convert Meters to Kilometres - judge url: https://judge.softuni.org/Contests/Practice/Index/1721#3
-
-
-distance_meters = int(input())
-distance_kilometers = distance_meters / 1000
-
-print(f'{distance_kilometers:.2f}')
-
