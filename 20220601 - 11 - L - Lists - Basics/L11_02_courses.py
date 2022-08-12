09d6db7384ba04f876bc74c54a6dc1849aa3506d
theterminal <kirilk777@gmail.com>
2022-08-12 23:29:05 +0300

    Added Problem Description

--- 20220601 - 11 - L - Lists - Basics/L11_02_courses.py
+++ 20220601 - 11 - L - Lists - Basics/L11_02_courses.py
@@ -2,7 +2,7 @@
 # 02 - Courses - judge url: https://judge.softuni.org/Contests/Practice/Index/1724#1
 
 
-# ---------- version 1 ---------------------
+# --------------------- version 1 -------------------------- judge 100%
 
 
 num_lines = int(input())
@@ -15,7 +15,7 @@ for _ in range(num_lines):
 print(course_list)
 
 
-# ---------- version 2 ---------------------
+# --------------------- version 2 -------------------------- judge 100%
 
 
 num_lines = int(input())
@@ -26,3 +26,48 @@ for _ in range(num_lines):
     course_list += [course_name_entered]
 
 print(course_list)
+
+
+""" ------------- Courses ---------------
+
+
+On the first line, you will receive a single number 'n'.
+On the following 'n' lines, you will receive names of courses.
+You should create a list of courses and print it.
+
+
+----------- Test Data --------------
+
+
+Input 1:
+-------
+2
+PB Python
+PF Python
+
+
+Output 1:
+--------
+['PB Python', 'PF Python']
+
+
+------------------------------------
+
+
+Input 2:
+-------
+4
+Front-End
+C# Web
+JS Core
+Programming Fundamentals
+
+
+Output 2:
+--------
+['Front-End', 'C# Web', 'JS Core', 'Programming Fundamentals']
+
+
+------------------------------------
+
+"""
