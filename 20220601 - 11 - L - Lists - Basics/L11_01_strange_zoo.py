49e31301c66de5f4c3ef5466314793f5906f99d8
theterminal <kirilk777@gmail.com>
2022-08-12 23:22:59 +0300

    Added Problem Description

--- 20220601 - 11 - L - Lists - Basics/L11_01_strange_zoo.py
+++ 20220601 - 11 - L - Lists - Basics/L11_01_strange_zoo.py
@@ -2,6 +2,9 @@
 # 01 - Strange Zoo - judge url: https://judge.softuni.org/Contests/Practice/Index/1724#0
 
 
+# --------------------- version 1 -------------------------- judge 100%
+
+
 str_tail = input()
 str_body = input()
 str_head = input()
@@ -19,3 +22,65 @@ print(test_animal)
 
 test_animal[0], test_animal[-1] = test_animal[-1], test_animal[0]
 print(test_animal)
+
+
+""" --------------- Strange Zoo ----------------
+
+
+You are at the zoo, and the meerkats look strange. 
+
+You will receive 3 strings on separate lines, representing the tail, the body, and the head of an animal in that order.
+Your task is to re-arrange the elements in a list so that the animal looks normal again:
+    •	On the first position is the head;
+    •	On the second position is the body;
+    •	On the last one is the tail.
+
+
+----------- Test Data -------------
+
+
+Input 1:
+-------
+my tail
+my body seems on place
+my head is on the wrong end!
+
+
+Output 1:
+--------
+['my head is on the wrong end!', 'my body seems on place', 'my tail']
+
+
+-----------------------------------
+
+
+Input 2:
+-------
+tail
+body
+head
+
+
+Output 2:
+--------
+['head', 'body', 'tail']
+
+
+-----------------------------------
+
+
+Input 3:
+-------
+T
+B
+H
+
+
+Output 3:
+--------
+['H', 'B', 'T']
+
+
+-----------------------------------
+
+"""
