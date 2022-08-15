cf51e52252d8b6dffee608dd88b587e103d17501
theterminal <kirilk777@gmail.com>
2022-08-15 20:45:35 +0300

    Problem description added

--- 20220603 - 12 - E - Lists - Basics/L12_06_survival_of_the_biggest.py
+++ 20220603 - 12 - E - Lists - Basics/L12_06_survival_of_the_biggest.py
@@ -1,8 +1,8 @@
-# 22020603 - Python Code - L12 - Lists Basics - Exercise
+# 22020603 - Python - L12 - Lists Basics - Exercise
 # 06 - Survival of The Biggest - judge url: https://judge.softuni.org/Contests/Compete/Index/1725#5
 
 
-# _____________ version 1 ________________
+# _____________ version 1 ________________ judge 100%
 
 
 lst_nums_in = list(map(int, input().split(' ')))
@@ -14,7 +14,7 @@ for k in range(nums_to_delete):
 print(', '.join([str(i) for i in lst_nums_in]))
 
 
-# _____________ version 2 ________________
+# _____________ version 2 ________________ judge 100%
 
 
 list_ints_in = input().split(' ')
@@ -36,7 +36,7 @@ list_strs = [str(i) for i in list_ints]
 print(', '.join(list_strs))
 
 
-# _____________ version 3 ________________
+# _____________ version 3 ________________ judge 100%
 
 
 list_str_in = input().split(' ')
@@ -49,3 +49,44 @@ for k in range(num_to_delete):
 
 list_strs = [str(i) for i in list_ints]
 print(', '.join(list_strs))
+
+
+""" __________________ Survival Of The Biggest _____________________
+
+
+Write a program that receives a list of integer numbers (separated by a single space) and a number 'n'.
+The number 'n' represents the count of numbers to remove from the list.
+You should remove the smallest ones, and then, you should print all the numbers that are left in the list,
+separated by a comma and a space ", ".
+
+
+______________________ Test Data _________________________
+
+
+Input 1:
+-------
+10 9 8 7 6 5
+3
+
+
+Output 1:
+--------
+10, 9, 8
+
+
+----------------------------------------------------------
+
+
+Input 2:
+-------
+1 10 2 9 3 8
+2
+
+
+Output 2:
+--------
+10, 9, 3, 8
+
+
+----------------------------------------------------------
+"""
