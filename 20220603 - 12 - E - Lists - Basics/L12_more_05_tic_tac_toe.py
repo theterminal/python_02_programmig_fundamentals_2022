ee4ed7120bda8b5ad271a997f1417fa2b9b76ff4
theterminal <kirilk777@gmail.com>
2022-08-16 09:39:47 +0300

    Problem description added

--- 20220603 - 12 - E - Lists - Basics/L12_more_05_tic_tac_toe.py
+++ 20220603 - 12 - E - Lists - Basics/L12_more_05_tic_tac_toe.py
@@ -2,6 +2,9 @@
 # 05 - Tic Tac Toe - judge url: https://judge.softuni.org/Contests/Practice/Index/1726#4
 
 
+# _________________ version 1 ___________________ judge 100%
+
+
 line_1 = list(map(int, input().split(' ')))
 line_2 = list(map(int, input().split(' ')))
 line_3 = list(map(int, input().split(' ')))
@@ -55,3 +58,68 @@ elif winner == 2:
     print('Second player won')
 else:
     print('Draw!')
+
+
+""" _______________ Tic Tac Toe ____________________
+
+
+You will receive a field of a tic-tac-toe game in three lines containing numbers, separated by a single space.
+
+Legend:
+•	0 - empty space
+•	1 - first player move
+•	2 - second player move
+
+Find out who the winner is.
+If the first player wins, print "First player won".
+If the second player wins, print "Second player won".
+Otherwise, print "Draw!".
+
+
+____________ Test Data _______________
+
+
+Input 1:
+-------
+2 0 1
+0 1 0
+1 0 2
+
+
+Output 1:
+--------
+First player won
+
+
+--------------------------------------
+
+
+Input 2:
+-------
+0 1 0
+2 2 2
+1 0 0
+
+
+Output 2:
+--------
+Second player won
+
+
+-------------------------------------
+
+
+Input 3:
+-------
+1 0 2
+0 1 2
+1 2 0
+
+
+Output 3:
+--------
+Draw!
+
+
+-------------------------------------
+"""
