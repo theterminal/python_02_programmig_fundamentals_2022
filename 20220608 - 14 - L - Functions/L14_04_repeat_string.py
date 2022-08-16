bca9e2bfefa97ee2d30a242fcb6556d144b7ddb7
theterminal <kirilk777@gmail.com>
2022-08-17 00:09:34 +0300

    Problem description added

--- 20220608 - 14 - L - Functions/L14_04_repeat_string.py
+++ 20220608 - 14 - L - Functions/L14_04_repeat_string.py
@@ -2,7 +2,7 @@
 # 04 - Repeat String - judge url: https://judge.softuni.org/Contests/Practice/Index/1727#3
 
 
-# ------- normal function ---------------------
+# ------- normal function --------------------- judge 100%
 
 
 def repeat_string(str_a1, counter_a2):
@@ -17,7 +17,7 @@ a2 = int(input())
 print(repeat_string(a1, a2))
 
 
-# ------- using lambda function ----------------
+# ------- using lambda function ---------------- judge 100%
 
 
 x1 = input()
@@ -25,3 +25,45 @@ counter_y = int(input())
 
 result_2 = lambda x, y: x * y
 print(result_2(x1, counter_y))
+
+
+""" ___________________ Repeat String ___________________
+
+
+Write a function that receives a string and a counter 'n'.
+The function should return a new string – the result of repeating the old string 'n' times.
+
+Print the result of the function.
+Try using lambda.
+
+
+____________ Test Data ____________
+
+
+Input 1:
+-------
+abc
+3
+
+
+Output 1:
+--------
+abcabcabc
+
+
+-----------------------------------
+
+
+Input 2:
+-------
+String
+2
+
+
+Output 2:
+--------
+StringString
+
+
+----------------------------------
+"""
