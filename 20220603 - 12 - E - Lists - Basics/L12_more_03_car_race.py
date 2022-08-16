992d47c19b340dc3a36153920e0affaa7a2b7b86
theterminal <kirilk777@gmail.com>
2022-08-16 09:35:18 +0300

    Problem description added

--- 20220603 - 12 - E - Lists - Basics/L12_more_03_car_race.py
+++ 20220603 - 12 - E - Lists - Basics/L12_more_03_car_race.py
@@ -1,5 +1,8 @@
-# 22020624 - Python Code - L12 - Lists Basics - More Exercise
+# 22020624 - Python - L12 - Lists Basics - More Exercise
 # 03 - Car Race - judge url: https://judge.softuni.org/Contests/Practice/Index/1726#2
+
+
+# __________________ version 1 ___________________ judge 100%
 
 
 nums_line = list(map(int, input().split(' ')))
@@ -35,3 +38,55 @@ else:
     winner = "No winner! Equal time!"
 
 print(f'The winner is {driver} with total time: {winner:.1f}')
+
+
+""" __________ Car Race ____________
+
+
+Write a program that announces the winner of a car race. 
+You will receive a sequence of numbers.
+Each number represents the time needed for the car to pass through that step (the index).
+
+There will be two cars.
+The first one starts from the left side, and the other one starts from the right side.
+The middle index of the sequence is the finish line.
+
+Calculate the total time each racer needs to reach the finish line and print the winner with his total time (the racer with less time).
+If you have a zero in the list, you should reduce the racer's time that reached it by 20% (from his current time).
+
+The number of elements in the sequence will always be odd.
+
+Print the result in the following format:
+"The winner is {left/right} with total time: {total_time}".
+
+The time should be formatted to the first decimal point.
+
+
+______________ Test Data ______________
+
+
+Input 1:
+-------
+29 13 9 0 13 0 21 0 14 82 12
+
+
+Output 1:
+--------
+The winner is left with total time: 53.8
+
+
+---------------------------------------
+
+
+Input 2:
+-------
+123 20 4 0 13 0 0 5 5 14 0
+
+
+Output 2:
+--------
+The winner is right with total time: 19.2
+
+
+---------------------------------------
+"""
