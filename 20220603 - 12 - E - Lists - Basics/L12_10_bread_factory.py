c65c4736ebe6f0a6dee7ee6c2c1f51e67a1c2e4f
theterminal <kirilk777@gmail.com>
2022-08-16 09:20:01 +0300

    Problem description added

--- 20220603 - 12 - E - Lists - Basics/L12_10_bread_factory.py
+++ 20220603 - 12 - E - Lists - Basics/L12_10_bread_factory.py
@@ -1,8 +1,8 @@
-# 22020613 - Python Code - L12 - Lists Basics - Exercise
+# 22020613 - Python - L12 - Lists Basics - Exercise
 # 10 - Bread Factory - https://judge.softuni.org/Contests/Compete/Index/1725#9
 
 
-# ------------- version 2 ---------------------------
+# ------------- version 2 --------------------------- judge 100%
 
 
 energy = coins = 100
@@ -55,7 +55,7 @@ if not flag:
     print(f'Energy: {energy}')
 
 
-# ------------- version 1 ---------------------------
+# ------------- version 1 --------------------------- judge 100%
 
 
 energy = coins = 100
@@ -106,3 +106,91 @@ if not flag:
     print('Day completed!')
     print(f'Coins: {coins}')
     print(f'Energy: {energy}')
+
+
+""" ________________ Bread Factory _________________
+
+
+As a young baker, you are baking the bread out of the bakery. 
+
+You have initial energy 100 and initial coins 100.
+You will be given a string representing the working day events.
+Each event is separated with '|' (vertical bar): "event1|event2| … eventN"
+Each event contains an event name or an ingredient and a number, separated by a dash ("{event/ingredient}-{number}")
+
+•	If the event is "rest":
+    o	You gain energy (the number in the second part). Note: your energy cannot exceed your initial energy (100).
+        Print: "You gained {gained_energy} energy.". 
+    o	After that, print your current energy: "Current energy: {current_energy}.".
+
+•	If the event is "order": 
+    o	You've earned some coins (the number in the second part).
+    o	Each time you get an order, your energy decreases by 30 points.
+        	If you have the energy to complete the order, print: "You earned {earned} coins.".
+        	Otherwise, skip the order and gain 50 energy points. Print: "You had to rest!".
+
+•	In any other case, you have an ingredient you should buy. The second part of the event contains the coins you should spend. 
+    o	If you have enough money, you should buy the ingredient and print: "You bought {ingredient}."
+    o	Otherwise, print "Closed! Cannot afford {ingredient}." and your bakery rush is over.
+
+If you managed to handle all events throughout the day, print on the following 3 lines: 
+"Day completed!"
+"Coins: {coins}"
+"Energy: {energy}"
+
+
+Input / Constraints:
+-------------------
+You will receive a string representing the working day events, separated with '|' (vertical bar) in the format:
+"event1|event2| … eventN".
+
+Each event contains an event name or an ingredient and a number, separated by a dash in the format:
+"{event/ingredient}-{number}"
+
+
+Output:
+------
+Print the corresponding messages described above.
+
+
+_________________ Test Data _________________
+
+
+Input 1:
+-------
+rest-2|order-10|eggs-100|rest-10
+
+
+Output 1:
+--------
+You gained 0 energy.
+Current energy: 100.
+You earned 10 coins.
+You bought eggs.
+You gained 10 energy.
+Current energy: 80.
+Day completed!
+Coins: 10
+Energy: 80
+
+
+--------------------------------------------
+
+
+Input 2:
+-------
+order-10|order-10|order-10|flour-100|order-100|oven-100|order-1000
+
+
+Output 2:
+--------
+You earned 10 coins.
+You earned 10 coins.
+You earned 10 coins.
+You bought flour.
+You had to rest!
+Closed! Cannot afford oven.
+
+
+--------------------------------------------
+"""
