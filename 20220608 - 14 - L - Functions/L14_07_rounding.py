db2765ebd97c9c46494d623f0a07ce58b8f60b3d
theterminal <kirilk777@gmail.com>
2022-08-17 00:10:01 +0300

    Problem description added

--- 20220608 - 14 - L - Functions/L14_07_rounding.py
+++ 20220608 - 14 - L - Functions/L14_07_rounding.py
@@ -2,6 +2,9 @@
 # 07 - Rounding - judge url: https://judge.softuni.org/Contests/Practice/Index/1727#6
 
 
+# -------------- version 1 --------------------- judge 100%
+
+
 def rounding(lst: list):
     lst_num = [round(float(i)) for i in lst_in]
 
@@ -12,7 +15,37 @@ lst_in = input().split(' ')
 print(rounding(lst_in))
 
 
-# -------------- version 2 ---------------------
+# -------------- version 2 --------------------- judge 100%
 
 
 print(list(map(lambda a: round(float(a)), input().split(' '))))
+
+
+""" ____________ Rounding _____________
+
+
+Input 1:
+-------
+1.0 2.5 3.0 4.5
+
+
+Output 1:
+--------
+[1, 2, 3, 4]
+
+
+---------------------------------------
+
+
+Input 2:
+-------
+2.56 1.9 -3.4 8.1
+
+
+Output 2:
+--------
+[3, 2, -3, 8]
+
+
+---------------------------------------
+"""
