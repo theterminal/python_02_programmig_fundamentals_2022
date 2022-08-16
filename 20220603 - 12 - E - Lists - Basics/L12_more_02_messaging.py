e5d076d82e233c935264680eb2d2ee14b82c5f93
theterminal <kirilk777@gmail.com>
2022-08-16 09:30:49 +0300

    Problem description added

--- 20220603 - 12 - E - Lists - Basics/L12_more_02_messaging.py
+++ 20220603 - 12 - E - Lists - Basics/L12_more_02_messaging.py
@@ -1,5 +1,8 @@
-# 22020613 - Python Code - L12 - Lists Basics - More Exercise
+# 22020613 - Python - L12 - Lists Basics - More Exercise
 # 02 - Messaging - judge url: https://judge.softuni.org/Contests/Practice/Index/1726#1
+
+
+# ______________ version 1 _________________ judge 100%
 
 
 nums_in = input().split(' ')
@@ -21,3 +24,51 @@ for i in nums_in:
     index = 0
 
 print(message)
+
+
+""" _______________ Messaging _______________
+
+
+On the first line, you will receive a sequence of numbers separated by a single space.
+On the second line, you will receive a string.
+
+Your task is to write a program that sends a message only using chars from the given string.
+Each char the program adds to the message should be found by its index.
+The index you are looking for is the sum of a number's digits from the sequence.
+If the index is greater than the length of the text, continue counting from the beginning (so that you always have a valid index).
+When you find a char, you should add it to the message and remove it from the string.
+It means that for the following index, the text will be with one character less.
+Print the final message.
+
+
+______________ Test Data _______________
+
+
+Input 1:
+-------
+9992 562 8933
+This is some message for you
+
+
+Output 1:
+--------
+hey
+
+
+----------------------------------------
+
+
+Input 2:
+-------
+2 122 1123 1321 9 17211
+87j973u59dg37e725!
+
+
+Output 2:
+--------
+judge!
+
+
+---------------------------------------
+"""
+
