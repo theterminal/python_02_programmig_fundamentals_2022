5d357ab8cd867de26870b518b4c6df9d00ec3b7b
theterminal <kirilk777@gmail.com>
2022-08-17 00:20:07 +0300

    Problem description added

--- 20220610 - 15 - E - Functions/L15_02_add_and_subtract.py
+++ 20220610 - 15 - E - Functions/L15_02_add_and_subtract.py
@@ -2,7 +2,7 @@
 # 02 - Add and Subtract - judge url: https://judge.softuni.org/Contests/Compete/Index/1728#1
 
 
-# ------------------ version 3 ------------------
+# ------------------ version 3 ------------------ judge 100%
 
 
 def sum_numbers(z1, z2):
@@ -24,7 +24,7 @@ z_3 = int(input())
 print(add_and_subtract(z_1, z_2, z_3))
 
 
-# ------------------ version 2 ------------------
+# ------------------ version 2 ------------------ judge 100%
 
 
 def sum_nums(z1, z2):
@@ -47,7 +47,7 @@ z_3 = int(input())
 print(add_and_subtract(z_1, z_2, z_3))
 
 
-# ------------------ version 1 ------------------
+# ------------------ version 1 ------------------ judge 100%
 
 
 def sum_nums_2(x1, x2):
@@ -69,3 +69,65 @@ b_b = int(input())
 c_c = int(input())
 
 print(add_and_subtract(a_a, b_b, c_c))
+
+
+""" _______________ Add & Subtract _______________
+
+
+You will receive three integer numbers.
+
+Write functions named:
+•	'sum_numbers()' that returns the sum of the first two integers
+•	'subtract()' that returns the difference between the returned result of the first function and the third integer
+
+Wrap the two functions in a function named 'add_and_subtract()' which will receive the three numbers as parameters.
+Print the result of the subtract() function on the console.
+
+
+___________ Test Data ____________
+
+
+Input 1:
+-------
+23
+6
+10
+
+
+Output 1:
+--------
+19
+
+
+----------------------------------
+
+
+Input 2:
+-------
+1
+17
+30
+
+
+Output 2:
+--------
+-12
+
+
+----------------------------------
+
+
+Input 3:
+-------
+42
+58
+100
+
+
+Output 3:
+--------
+0
+
+
+----------------------------------
+"""
