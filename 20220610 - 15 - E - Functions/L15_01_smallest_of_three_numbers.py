33380ac7634ec2c2acbdccde787f40f81e0fcd8d
theterminal <kirilk777@gmail.com>
2022-08-17 00:15:18 +0300

    Problem description added

--- 20220610 - 15 - E - Functions/L15_01_smallest_of_three_numbers.py
+++ 20220610 - 15 - E - Functions/L15_01_smallest_of_three_numbers.py
@@ -2,7 +2,7 @@
 # 01 - Smallest of Three Numbers - judge url: https://judge.softuni.org/Contests/Compete/Index/1728#0
 
 
-# --------- version 1 ----------------------------------
+# --------- version 1 ---------------------------------- judge 100%
 
 
 def smallest_of_three_numbers(a: int, b: int, c: int):
@@ -17,7 +17,7 @@ p3 = int(input())
 print(smallest_of_three_numbers(p1, p2, p3))
 
 
-# --------- version 2 ----------------------------------
+# --------- version 2 ---------------------------------- judge 100%
 
 
 def smallest_of_three_numbers(a: int, b: int, c: int):
@@ -30,7 +30,7 @@ p3 = int(input())
 print(smallest_of_three_numbers(p1, p2, p3))
 
 
-# --------- version 3 ----------------------------------
+# --------- version 3 ---------------------------------- judge 100%
 
 
 def smallest_of_three_numbers(lst_nums):
@@ -44,3 +44,60 @@ p3 = int(input())
 lst_all_nums = [p1, p2, p3]
 min_num = smallest_of_three_numbers(lst_all_nums)
 print(min_num)
+
+
+""" _____________ Smallest Of Three Numbers ________________
+
+
+Write a function that receives three integer numbers and returns the smallest.
+Print the result on the console.
+Use an appropriate name for the function.
+
+
+________ Test Data _________
+
+
+Input 1:
+-------
+2
+5
+3
+
+
+Output 1:
+--------
+2
+
+
+----------------------------
+
+
+Input 2:
+-------
+600
+342
+123
+
+
+Output 2:
+--------
+123
+
+
+----------------------------
+
+
+Input 3:
+-------
+25
+21
+4
+
+
+Output 3:
+--------
+4
+
+
+----------------------------
+"""
