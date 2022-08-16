bcd08968d2976a9ac283e4139042d971c554fa6f
theterminal <kirilk777@gmail.com>
2022-08-16 08:54:16 +0300

    Problem description added

--- 20220603 - 12 - E - Lists - Basics/L12_07_easter_gifts.py
+++ 20220603 - 12 - E - Lists - Basics/L12_07_easter_gifts.py
@@ -1,8 +1,8 @@
-# 22020603 - Python Code - L12 - Lists Basics - Exercise
+# 22020603 - Python - L12 - Lists Basics - Exercise
 # 07 - Easter Gifts - https://judge.softuni.org/Contests/Compete/Index/1725#6
 
 
-# ______________ version 5 __________________100% judge
+# ______________ version 5 _________________ judge 100%
 
 
 lst_gifts = list(map(str, input().split(' ')))
@@ -35,7 +35,7 @@ lst_gifts = [i for i in lst_gifts if i != 'None']
 print(' '.join(lst_gifts))
 
 
-# ______________ version 4 __________________100% judge
+# ______________ version 4 __________________ judge 100%
 
 
 gifts = input().split(' ')
@@ -66,7 +66,7 @@ for j in gifts:
 print(' '.join(gifts))
 
 
-# ______________ version 3 __________________100% judge
+# ______________ version 3 __________________ judge 100%
 
 
 gifts = input().split(' ')
@@ -96,7 +96,7 @@ while 'None' in gifts:
 print(' '.join(gifts))
 
 
-# ______________ version 2 __________________100 % in judge
+# ______________ version 2 __________________ judge 100%
 
 
 gifts = input().split(' ')
@@ -126,7 +126,7 @@ for j in range(len(gifts)):
 print(' '.join(gifts))
 
 
-# ______________ version 1 __________________ 100% in judge
+# ______________ version 1 __________________ judge 100%
 
 
 gifts = input().split(' ')
@@ -155,3 +155,82 @@ for j in range(len(gifts)):
         gifts.remove('None')
 
 print(' '.join(gifts))
+
+
+""" _______________ Easter Gifts __________________
+
+
+As a good friend, you decide to buy presents for your friends.
+Create a program that helps you plan the gifts for your friends and family.
+
+First, you are going to receive the gifts you plan on buying on a single line, separated by space, in the following format:
+
+"{gift1} {gift2} {gift3}… {giftn}"
+
+Then you will start receiving commands until you read the "No Money" message.
+There are three possible commands:
+
+    •	"OutOfStock {gift}"
+        o	Find the gifts with this name in your collection, if any, and change their values to "None".
+        
+    •	"Required {gift} {index}"
+        o	If the index is valid, replace the gift on the given index with the given gift.
+        
+    •	"JustInCase {gift}"
+        o	Replace the value of your last gift with this one.
+
+In the end, print the gifts on a single line, except the ones with value "None", separated by a single space in the following format:
+
+"{gift1} {gift2} {gift3} … {giftn}"
+
+
+Input / Constraints:
+-------------------
+    •	On the 1st line,  you will receive the names of the gifts, separated by a single space.
+    •	On the following lines, until the "No Money" command is received, you will be receiving commands.
+    •	The input will always be valid.
+
+
+Output:
+------
+    •	Print the gifts in the format described above.
+
+
+___________________ Test Data _____________________
+
+
+Input 1:
+-------
+Eggs StuffedAnimal Cozonac Sweets EasterBunny Eggs Clothes
+OutOfStock Eggs
+Required Spoon 2
+JustInCase ChocolateEgg
+No Money
+
+
+Output 1:
+--------
+StuffedAnimal Spoon Sweets EasterBunny ChocolateEgg
+
+
+---------------------------------------------------
+
+
+Input 2:
+-------
+Sweets Cozonac Clothes Flowers Wine Clothes Eggs Clothes
+Required Paper 8
+OutOfStock Clothes
+Required Chocolate 2
+JustInCase Hat
+OutOfStock Cable
+No Money
+
+
+Output 2:
+--------
+Sweets Cozonac Chocolate Flowers Wine Eggs Hat
+
+
+---------------------------------------------------
+"""
