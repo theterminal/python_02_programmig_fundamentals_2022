0bfe42fcecf06d8f1b6b2f9d670707f38d622424
theterminal <kirilk777@gmail.com>
2022-08-16 09:25:06 +0300

    Problem description added

--- 20220603 - 12 - E - Lists - Basics/L12_more_01_zeros_to_back.py
+++ 20220603 - 12 - E - Lists - Basics/L12_more_01_zeros_to_back.py
@@ -1,8 +1,8 @@
-# 22020613 - Python Code - L12 - Lists Basics - More Exercise
+# 22020613 - Python - L12 - Lists Basics - More Exercise
 # 01 - Zeros to Back - judge url: https://judge.softuni.org/Contests/Practice/Index/1726#0
 
 
-# --------------------- version 1 ---------------------------
+# --------------------- version 1 --------------------------- judge 100%
 
 
 lst_nums = [int(i) for i in input().split(', ')]
@@ -15,7 +15,7 @@ for i in lst_nums:
 print(lst_nums)
 
 
-# --------------------- version 2 ---------------------------
+# --------------------- version 2 --------------------------- judge 100%
 
 
 lst_1 = list(map(int, input().split(', ')))
@@ -26,3 +26,42 @@ for i in lst_1:
         lst_1.append(0)
 
 print(lst_1)
+
+
+""" _______________ Zeros To Back __________________
+
+
+Write a program that receives a single string (integers separated by a comma and space ", "),
+finds all the zeros, and moves them to the back without messing up the other elements.
+
+Print the resulting integer list.
+
+
+_____________ Test Data _____________
+
+
+Input 1:
+--------
+1, 0, 1, 2, 0, 1, 3
+
+
+Output 1:
+---------
+[1, 1, 2, 1, 3, 0, 0]
+
+
+-------------------------------------
+
+
+Input 2:
+-------
+0, 5, 0, 4, 0, 0, 5
+
+
+Output 2:
+--------
+[5, 4, 5, 0, 0, 0, 0]
+
+
+-------------------------------------
+"""
