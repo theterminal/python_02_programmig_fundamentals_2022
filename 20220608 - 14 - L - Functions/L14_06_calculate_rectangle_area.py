853d04915557f6132812f26eb306d7b6b012ee94
theterminal <kirilk777@gmail.com>
2022-08-17 00:09:53 +0300

    Problem description added

--- 20220608 - 14 - L - Functions/L14_06_calculate_rectangle_area.py
+++ 20220608 - 14 - L - Functions/L14_06_calculate_rectangle_area.py
@@ -10,3 +10,42 @@ w = int(input())
 h = int(input())
 
 print(rectangle_area(w, h))
+
+
+""" __________________ Calculating Rectangle Area ______________________
+
+
+Create a function that calculates and returns the area of a rectangle by given width and height.
+Print the result on the console.
+
+
+____________ Test Data ______________
+
+
+Input 1:
+-------
+3
+4
+
+
+Output 1:
+--------
+12
+
+
+-------------------------------------
+
+
+Input 2:
+-------
+6
+2
+
+
+Output 2:
+--------
+12
+
+
+------------------------------------
+"""
