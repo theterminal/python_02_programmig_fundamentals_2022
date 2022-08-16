9a6f7eb862a299224dec1490ad693c4cfb36750d
theterminal <kirilk777@gmail.com>
2022-08-17 00:08:58 +0300

    Problem description added

--- 20220608 - 14 - L - Functions/L14_02_grades.py
+++ 20220608 - 14 - L - Functions/L14_02_grades.py
@@ -2,6 +2,9 @@
 # 02 - Grades - judge url: https://judge.softuni.org/Contests/Practice/Index/1727#1
 
 
+# --------------------------- version 1 -------------------------- judge 100%
+
+
 def grade_test(grade):
 
     if 2.00 <= grade < 3:
@@ -18,3 +21,58 @@ def grade_test(grade):
 
 entry = float(input())
 print(grade_test(entry))
+
+
+""" ______________ Grades ______________
+
+
+Write a function that receives a grade between 2.00 and 6.00 and prints the corresponding grade in words.
+
+•	2.00 – 2.99 - "Fail"
+•	3.00 – 3.49 - "Poor"
+•	3.50 – 4.49 - "Good"
+•	4.50 – 5.49 - "Very Good"
+•	5.50 – 6.00 - "Excellent"
+
+
+____________ Test Data ____________
+
+
+Input 1:
+-------
+3.33
+
+
+Output 1:
+--------
+Poor
+
+
+----------------------------------
+
+
+Input 2:
+-------
+4.50
+
+
+Output 2:
+--------
+Very Good
+
+
+----------------------------------
+
+
+Input 3:
+-------
+2.99
+
+
+Output 3:
+--------
+Fail
+
+
+----------------------------------
+"""
