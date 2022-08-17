cfdac2979ab80a1250ca24dc10ecad1585c0d0f0
theterminal <kirilk777@gmail.com>
2022-08-18 00:20:55 +0300

    Problem description added

--- 20220610 - 15 - E - Functions/L15_07_min_max_sum.py
+++ 20220610 - 15 - E - Functions/L15_07_min_max_sum.py
@@ -2,7 +2,7 @@
 # 07 - Mim Max and Sum - judge url: https://judge.softuni.org/Contests/Compete/Index/1728#6
 
 
-# ------------------- version 2 -----------------------
+# ------------------- version 2 ----------------------- judge 100%
 
 
 lst_nums = [int(i) for i in input().split()]
@@ -11,7 +11,7 @@ print(f'The maximum number is {max(lst_nums)}')
 print(f'The sum number is: {sum(lst_nums)}')
 
 
-# ------------------- version 1 -----------------------
+# ------------------- version 1 ----------------------- judge 100%
 
 
 def min_max_sum(lst_ints: list):
@@ -25,3 +25,51 @@ def min_max_sum(lst_ints: list):
 lst_nums = [int(i) for i in input().split()]
 result = min_max_sum(lst_nums)
 print(result)
+
+
+""" ____________ Min Max and Sum _____________
+
+
+Write a program that receives a sequence of numbers (integers) separated by a single space.
+It should print the min and max values of the given numbers and the sum of all the numbers in the list.
+
+Use min(), max() and sum().
+
+The output should be as follows:
+    •	On the first line: "The minimum number is {minimum number}"
+    •	On the second line: "The maximum number is {maximum number}"
+    •	On the third line: "The sum number is: {sum of all numbers}"
+
+
+__________ Test Data ___________
+
+
+Input 1:
+-------
+2 4 6
+
+
+Output 1:
+--------
+The minimum number is 2
+The maximum number is 6
+The sum number is: 12
+
+
+--------------------------------
+
+
+Input 2:
+-------
+12 52 11 53 2 8 45
+
+
+Output 2:
+--------
+The minimum number is 2
+The maximum number is 53
+The sum number is: 183
+
+
+-------------------------------
+"""
