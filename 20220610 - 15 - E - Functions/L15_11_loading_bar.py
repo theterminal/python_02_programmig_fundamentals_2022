65694ad69c919567348eabc2de8a0377cac4ec63
theterminal <kirilk777@gmail.com>
2022-08-18 00:21:35 +0300

    Problem description added

--- 20220610 - 15 - E - Functions/L15_11_loading_bar.py
+++ 20220610 - 15 - E - Functions/L15_11_loading_bar.py
@@ -2,6 +2,9 @@
 # 11 - Loading Bar - judge url: https://judge.softuni.org/Contests/Compete/Index/1728#10
 
 
+# ---------------------- version 1 -------------------- judge 100%
+
+
 def loading_bar(num):
     if num == 100:
         return f'{num}% Complete!\n[%%%%%%%%%%]'
@@ -14,3 +17,60 @@ def loading_bar(num):
 
 int_input_div_10 = int(input())
 print(loading_bar(int_input_div_10))
+
+
+""" ______________ Loading Bar _______________
+
+
+You will receive a single integer number between 0 and 100 (inclusive) divisible by 10 without remainder (0, 10, 20, 30...).
+
+Your task is to create a function that returns a loading bar depending on the number you have received in the input.
+Print the result on the console.
+
+For more clarification, see the examples below.
+
+
+_____________ Test Data _______________
+
+
+Input 1:
+-------
+30
+
+
+Output 1:
+--------
+30% [%%%.......]
+Still loading...
+
+
+-----------------------------
+
+
+Input 2:
+-------
+50
+
+
+Output 2:
+--------
+50% [%%%%%.....]
+Still loading...
+
+
+-----------------------------
+
+
+Input 3:
+-------
+100
+
+
+Output 3:
+--------
+100% Complete!
+[%%%%%%%%%%]
+
+
+-----------------------------
+"""
