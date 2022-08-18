112bcf600b89839c823ca2cba9a0c437171f2377
theterminal <kirilk777@gmail.com>
2022-08-19 00:26:58 +0300

    Problem description added

--- 20220610 - 15 - E - Functions/L15_more_04_tribonacci_sequence.py
+++ 20220610 - 15 - E - Functions/L15_more_04_tribonacci_sequence.py
@@ -2,6 +2,9 @@
 # 04 - Tribonacci Sequence - judge url: https://judge.softuni.org/Contests/Practice/Index/1729#3
 
 
+# -------------- version 1 -------------------------- judge 100%
+
+
 def tribonacci(num):
     output = [1]
 
@@ -14,3 +17,43 @@ num_end = int(input())
 result = tribonacci(num_end)
 
 print(' '.join(result))
+
+
+""" _______________ Tribonacci Sequence __________________
+
+
+In the Tribonacci sequence, every number is formed by the sum of the previous 3.
+Write a function that prints numbers from the Tribonacci sequence on one line separated by a single space,
+starting from 1.
+
+You will receive a positive integer number as input.
+
+
+_________ Test Data ___________
+
+
+Input 1:
+--------
+4
+
+
+Output 1:
+--------
+1 1 2 4
+
+
+-------------------------------
+
+
+Input 2:
+-------
+8
+
+
+Output 2:
+--------
+1 1 2 4 7 13 24 44
+
+
+-------------------------------
+"""
