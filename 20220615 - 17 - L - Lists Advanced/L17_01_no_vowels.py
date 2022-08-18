f7124d5ac00c2f2f767c6eaaab41e84e6a45ae99
theterminal <kirilk777@gmail.com>
2022-08-19 00:42:09 +0300

    Problem description added

--- 20220615 - 17 - L - Lists Advanced/L17_01_no_vowels.py
+++ 20220615 - 17 - L - Lists Advanced/L17_01_no_vowels.py
@@ -1,5 +1,8 @@
-# 20220616 - Python Code - Lists Advance - Lecture
+# 20220616 - Python - Lists Advance - Lecture
 # 01 - No Vowels - judge: https://judge.softuni.org/Contests/Practice/Index/1730#0
+
+
+# ----------------- version 1 ------------------- judge 100%
 
 
 str_in = input()
@@ -8,3 +11,41 @@ nots = ['a', 'o', 'u', 'e', 'i']
 result = [i for i in str_in if i.lower() not in nots]
 
 print(''.join(result))
+
+
+""" _____________ No Vowels ______________
+
+
+Using comprehension, write a program that receives a text and removes all its vowels, case insensitive.
+Print the new text string after removing the vowels.
+The vowels that should be considered are 'a', 'o', 'u', 'e', 'i'.
+
+
+___________ Test Data _______________
+
+
+Input 1:
+-------
+Python
+
+
+Output 1:
+--------
+Pythn
+
+
+------------------------------------
+
+
+Input 2:
+-------
+ILovePython
+
+
+Output 2:
+--------
+LvPythn
+
+
+-----------------------------------
+"""
