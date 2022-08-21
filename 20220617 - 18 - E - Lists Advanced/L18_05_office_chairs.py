145d42bc0a9424e79ed14b8cc05e91a88761e8c1
theterminal <kirilk777@gmail.com>
2022-08-22 00:09:46 +0300

    Problem description added

--- 20220617 - 18 - E - Lists Advanced/L18_05_office_chairs.py
+++ 20220617 - 18 - E - Lists Advanced/L18_05_office_chairs.py
@@ -1,8 +1,8 @@
-# 20220617 - Python Code - Lists Advance - Exercise
+# 20220617 - Python - Lists Advance - Exercise
 # 05 - Office Chairs - judge: https://judge.softuni.org/Contests/Compete/Index/1731#4
 
 
-# ---------------- version 3 ----------------------
+# ---------------- version 3 ---------------------- judge 100%
 
 
 def check_chairs(num_rooms):
@@ -29,7 +29,7 @@ if total_available_chairs >= need_chairs:
     print(f'Game On, {total_available_chairs} free chairs left')
 
 
-# ---------------- version 2 ----------------------
+# ---------------- version 2 ---------------------- judge 100%
 
 
 num_rooms = int(input())
@@ -52,7 +52,7 @@ if not flag:
     print(f'Game On, {total_free_chairs} free chairs left')
 
 
-# ---------------- version 1 ----------------------
+# ---------------- version 1 ---------------------- judge 100%
 
 
 num_rooms = int(input())
@@ -76,3 +76,61 @@ total_free_chairs = total_chairs - total_people
 
 if not flag:
     print(f'Game On, {total_free_chairs} free chairs left')
+
+
+""" ____________ Office Chairs ____________
+
+
+You are a facility manager at a large business center.
+One of your responsibilities is to check if each conference room in the center has enough chairs for the visitors.
+
+On the first line, you will be given an integer 'n' representing the number of rooms in the business center.
+On the following 'n' lines for each room, you will receive information about the chairs in the room and the number of visitors.
+Each chair will be presented with the char "X".
+Next, there will be a single space and the number of visitors at the end.
+For example: "XXXXX 4" (5 chairs and 4 visitors).
+
+Keep track of the free chairs:
+    •	If there are not enough chairs in a specific room, print the following message:
+        "{needed_chairs_in_room} more chairs needed in room {number_of_room}".
+        The rooms start from 1.
+        
+    •	Otherwise, print: "Game On, {total_free_chairs} free chairs left".
+
+
+__________ Test Data ___________
+
+
+Input 1:
+-------
+4
+XXXX 4
+XX 1
+XXXXXX 3
+XXX 3
+
+
+Output 1:
+--------
+Game On, 4 free chairs left
+
+
+-------------------------------
+
+
+Input 2:
+-------
+3
+XXXXXXX 5
+XXXX 5
+XXXXXX 8
+
+
+Output 2:
+--------
+1 more chairs needed in room 2
+2 more chairs needed in room 3
+
+
+-------------------------------
+"""
