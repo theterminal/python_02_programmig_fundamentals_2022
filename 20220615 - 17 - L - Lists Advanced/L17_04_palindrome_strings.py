697065eb06b39a3c7aedce26306b06df0659766a
theterminal <kirilk777@gmail.com>
2022-08-21 17:07:11 +0300

    Problem description added

--- 20220615 - 17 - L - Lists Advanced/L17_04_palindrome_strings.py
+++ 20220615 - 17 - L - Lists Advanced/L17_04_palindrome_strings.py
@@ -1,8 +1,8 @@
-# 20220617 - Python Code - Lists Advance - Lecture
+# 20220617 - Python - Lists Advance - Lecture
 # 04 - Palindrome Strings - judge: https://judge.softuni.org/Contests/Practice/Index/1730#3
 
 
-# ----------------- version 1 -------------------------
+# ----------------- version 1 ------------------------- judge 100%
 
 
 words_in = input().split(' ')
@@ -21,7 +21,7 @@ print(palindrome_out)
 print(f'Found palindrome {number} times')
 
 
-# ----------------- version 2 -------------------------
+# ----------------- version 2 ------------------------- judge 100%
 
 
 def palindrome_filter(x):
@@ -35,3 +35,48 @@ palindrome = input()
 palindrome_list = [word for word in words if palindrome_filter(word)]
 print(palindrome_list)
 print(f'Found palindrome {palindrome_list.count(palindrome)} times')
+
+
+""" _______________ Palindrome Strings _______________
+
+
+On the first line, you will receive words separated by a single space.
+On the second line, you will receive a palindrome.
+
+First, you should print a list containing all the found palindromes in the sequence.
+Then, you should print the number of occurrences of the given palindrome in the format:
+"Found palindrome {number} times".
+
+
+__________ Test Data ____________
+
+
+Input 1:
+-------
+wow father mom wow shirt stats
+wow
+
+
+Output 1:
+--------
+['wow', 'mom', 'wow', 'stats']
+Found palindrome 2 times
+
+
+---------------------------------
+
+
+Input 2:
+-------
+hey how you doin? lol
+mom
+
+
+Output 2:
+--------
+['lol']
+Found palindrome 0 times
+
+
+--------------------------------
+"""
