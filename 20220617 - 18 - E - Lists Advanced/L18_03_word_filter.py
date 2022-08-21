b7f2c6d859276288ac70888a95547c80e5a3f202
theterminal <kirilk777@gmail.com>
2022-08-21 18:57:56 +0300

    Problem description added

--- 20220617 - 18 - E - Lists Advanced/L18_03_word_filter.py
+++ 20220617 - 18 - E - Lists Advanced/L18_03_word_filter.py
@@ -2,7 +2,7 @@
 # 03 - Word Filter - judge: https://judge.softuni.org/Contests/Compete/Index/1731#2
 
 
-# -------------- version 1 ----------------------------------
+# -------------- version 1 ---------------------------------- judge 100%
 
 
 lst_text = input().split(' ')
@@ -11,14 +11,14 @@ for i in [j for j in lst_text if len(j) % 2 == 0]:
     print(i)
 
 
-# -------------- version 2 ----------------------------------
+# -------------- version 2 ---------------------------------- judge 100%
 
 
 for i in [j for j in input().split(' ') if len(j) % 2 == 0]:
     print(i)
 
 
-# -------------- version 3 ----------------------------------
+# -------------- version 3 ---------------------------------- judge 100%
 
 
 print('\n'.join([i for i in input().split() if len(i) % 2 == 0]))
