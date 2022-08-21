597c1bba9e6a772c1d48a1f14f4c7598bcafad86
theterminal <kirilk777@gmail.com>
2022-08-22 00:09:17 +0300

    Problem description added

--- 20220617 - 18 - E - Lists Advanced/L18_04_number_classification.py
+++ 20220617 - 18 - E - Lists Advanced/L18_04_number_classification.py
@@ -1,8 +1,8 @@
-# 20220617 - Python Code - Lists Advance - Exercise
+# 20220617 - Python - Lists Advance - Exercise
 # 04 - Number Classification - judge: https://judge.softuni.org/Contests/Compete/Index/1731#3
 
 
-# ------------------------ version 1 ----------------------------------
+# ------------------------ version 1 ---------------------------------- judge 100%
 
 
 lst_int_input = list(map(int, input().split(', ')))
@@ -13,7 +13,7 @@ print(f'Even: {", ".join([str(i) for i in lst_int_input if i % 2 == 0])}')
 print(f'Odd: {", ".join([str(i) for i in lst_int_input if i % 2 == 1])}')
 
 
-# ------------------------- version 2 ---------------------------------
+# ------------------------- version 2 --------------------------------- judge 100%
 
 
 def positive_numbers(lst_nums):
@@ -38,3 +38,48 @@ print(f'Positive: {", ".join(positive_numbers(lst_int_in))}')
 print(f'Negative: {", ".join(negative_numbers(lst_int_in))}')
 print(f'Even: {", ".join(even_numbers(lst_int_in))}')
 print(f'Odd: {", ".join(odd_numbers(lst_int_in))}')
+
+
+""" ______________ Number Clssification ________________
+
+
+Using a list comprehension, write a program that receives numbers, separated by comma and space ", ",
+and prints all the positive, negative, even, and odd numbers on separate lines as shown below.
+
+Note: Zero is counted for a positive number
+
+
+________ Test Data ___________
+
+
+Input 1:
+-------
+1, -2, 0, 5, 3, 4, -100, -20, 12, 19, -33
+
+
+Output 1:
+--------
+Positive: 1, 0, 5, 3, 4, 12, 19
+Negative: -2, -100, -20, -33
+Even: -2, 0, 4, -100, -20, 12
+Odd: 1, 5, 3, 19, -33
+
+
+------------------------------
+
+
+Input 2:
+-------
+1, 2, 53, 2, 21
+
+
+Output 2:
+--------
+Positive: 1, 2, 53, 2, 21
+Negative:
+Even: 2, 2
+Odd: 1, 53, 21
+
+
+------------------------------
+"""
