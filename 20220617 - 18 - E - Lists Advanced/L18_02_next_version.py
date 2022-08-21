95abd9b3b0a4c9fc4a1a74c2f338a0a8923d34db
theterminal <kirilk777@gmail.com>
2022-08-21 18:55:12 +0300

    Problem description added

--- 20220617 - 18 - E - Lists Advanced/L18_02_next_version.py
+++ 20220617 - 18 - E - Lists Advanced/L18_02_next_version.py
@@ -2,7 +2,7 @@
 # 02 - Next Version - judge: https://judge.softuni.org/Contests/Compete/Index/1731#1
 
 
-# ------------------ version 4 ------------------------
+# ------------------ version 4 ------------------------ judge 100%
 # This version will add a number above version 9.9.9  -  it'll become 1.0.0.0 and NOT 10.9.9
 
 
@@ -15,7 +15,7 @@ for i in version:
 print(result[:-1:])
 
 
-# ------------------ version 3 ------------------------
+# ------------------ version 3 ------------------------ judge 100%
 
 
 version = [i for i in input().split('.')]
@@ -24,7 +24,7 @@ version = str(int(version[0] + version[1] + version[2]) + 1)
 print('.'.join(version[i] for i in range(len(version))))
 
 
-# ------------------- version 2 -----------------------
+# ------------------- version 2 ----------------------- judge 100%
 
 
 version = [int(i) for i in input().split('.')]
@@ -39,7 +39,7 @@ for i in range(len(version) - 1, -1, -1):
 print('.'.join(i for i in [str(i) for i in version]))
 
 
-# ------------------ version 1 ------------------------
+# ------------------ version 1 ------------------------ judge 100%
 
 
 version = [int(i) for i in input().split('.')]
