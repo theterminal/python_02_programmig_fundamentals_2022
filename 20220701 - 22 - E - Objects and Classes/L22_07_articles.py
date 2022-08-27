b59289976ecebbb75804935b9d1a7552c95fd8d4
theterminal <kirilk777@gmail.com>
2022-08-27 20:56:35 +0300

    Refactoring

--- 20220701 - 22 - E - Objects and Classes/L22_07_articles.py
+++ 20220701 - 22 - E - Objects and Classes/L22_07_articles.py
@@ -1,5 +1,8 @@
-# 20220701 - Python Code - Objects and Classes - Exercise
+# 20220701 - Python - Objects and Classes - Exercise
 # 05 - Class - judge url: https://judge.softuni.org/Contests/Compete/Index/1734#6
+
+
+# ------------------------- version 1 ---------------------------- judge 100%
 
 
 class Article:
