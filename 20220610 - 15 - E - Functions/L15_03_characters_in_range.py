0f60929e86d23da40a3b93d144143afc66ebcf4a
theterminal <kirilk777@gmail.com>
2022-09-11 17:09:41 +0300

    Refactoring

--- 20220610 - 15 - E - Functions/L15_03_characters_in_range.py
+++ 20220610 - 15 - E - Functions/L15_03_characters_in_range.py
@@ -1,4 +1,4 @@
-# 20220614 - Python Code - Functions - Exercise
+# 20220614 - Python - Functions - Exercise
 # 03 - Characters in Range - judge url: https://judge.softuni.org/Contests/Compete/Index/1728#2
 
 
