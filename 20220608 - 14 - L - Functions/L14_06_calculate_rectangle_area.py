f92c7e2f66f89ae7fbddf2f97726b53fb4ee9f0b
theterminal <kirilk777@gmail.com>
2022-09-11 17:07:35 +0300

    Refactoring

--- 20220608 - 14 - L - Functions/L14_06_calculate_rectangle_area.py
+++ 20220608 - 14 - L - Functions/L14_06_calculate_rectangle_area.py
@@ -2,6 +2,9 @@
 # 06 - Calculate Rectangle Area - judge url: https://judge.softuni.org/Contests/Practice/Index/1727#5
 
 
+# -------------- version 1 --------------------- judge 100%
+
+
 def rectangle_area(width: int, height: int):
     return width * height
 
