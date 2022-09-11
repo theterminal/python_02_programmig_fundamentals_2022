e4c4d1078dff15e307df1d4f86f8dfab7983082a
theterminal <kirilk777@gmail.com>
2022-09-11 17:10:35 +0300

    Refactoring

--- 20220610 - 15 - E - Functions/L15_09_password_validator.py
+++ 20220610 - 15 - E - Functions/L15_09_password_validator.py
@@ -1,4 +1,4 @@
-# 20220614 - Python Code - Functions - Exercise
+# 20220614 - Python - Functions - Exercise
 # 09 - Password Validator - judge url: https://judge.softuni.org/Contests/Compete/Index/1728#8
 
 
