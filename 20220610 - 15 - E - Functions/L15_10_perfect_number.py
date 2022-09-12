e80956734b50dd568141aa7434940be3d950dfcf
theterminal <kirilk777@gmail.com>
2022-09-12 07:06:37 +0300

    Refactoring

--- 20220610 - 15 - E - Functions/L15_10_perfect_number.py
+++ 20220610 - 15 - E - Functions/L15_10_perfect_number.py
@@ -1,4 +1,4 @@
-# 20220614 - Python Code - Functions - Exercise
+# 20220614 - Python - Functions - Exercise
 # 10 - Perfect Number - judge url: https://judge.softuni.org/Contests/Compete/Index/1728#9
 
 
