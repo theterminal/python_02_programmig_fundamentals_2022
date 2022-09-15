7b05b9ee2f1215bee39a6f1d830de1fab3977618
theterminal <kirilk777@gmail.com>
2022-09-15 08:04:06 +0300

    Initial commit

---
+++ 20220704 - 23 - L - Software Development Concepts - Part 2/01 - HTML, CSS, JS - example - calculator/javascript.js
@@ -0,0 +1,14 @@
+document.getElementById("calc-12").onclick =
+            function () {
+                document.getElementById("result-12").value =
+                    Number(document.getElementById("num-1").value) +
+                    Number(document.getElementById("num-2").value);
+            }
+
+
+document.getElementById("calc-34").onclick =
+            function () {
+                document.getElementById("result-34").value =
+                    Number(document.getElementById("num-3").value) *
+                    Number(document.getElementById("num-4").value);
+            }
