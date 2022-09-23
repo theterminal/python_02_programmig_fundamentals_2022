182e087a7d3fb430d364d743f0e4dfef8d253363
theterminal <kirilk777@gmail.com>
2022-09-23 07:41:55 +0300

    Refactoring

--- 20220715 - 28 - E - Text Processing/L28_04_caesar_cipher.py
+++ 20220715 - 28 - E - Text Processing/L28_04_caesar_cipher.py
@@ -1,4 +1,4 @@
-# 20220715 - Python Code - String Processing - Exercise
+# 20220715 - Python - String Processing - Exercise
 # 04 - Caesar Cipher - judge url: https://judge.softuni.org/Contests/Compete/Index/1740#3
 
 
