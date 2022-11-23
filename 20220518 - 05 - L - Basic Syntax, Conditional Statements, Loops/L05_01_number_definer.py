18f8ac47bf818ef269bd037e9a990bef5c38aea7
theterminal <kirilk777@gmail.com>
2022-11-23 07:48:01 +0200

    Refactoring

--- 20220518 - 05 - L - Basic Syntax, Conditional Statements, Loops/L05_01_number_definer.py
+++ 20220518 - 05 - L - Basic Syntax, Conditional Statements, Loops/L05_01_number_definer.py
@@ -2,9 +2,6 @@
 # 01 - Number Definer - judge url: https://judge.softuni.org/Contests/Practice/Index/1718#0
 
 
-# -------------------------------- version 1 ------------------------------------ judge: 100%
-
-
 num_entered = float(input())
 
 if num_entered == 0:
@@ -23,64 +20,3 @@ else:
         print('large negative')
     else:
         print('negative')
-
-
-""" ------------------- Number Definer ----------------------
-
-
-Write a program that reads a floating-point number and:
-
--	prints "zero" if the number is zero
--	prints "positive" or "negative"
--	adds "small" if the absolute value of the number is less than 1 and it is not 0, or "large" if it exceeds 1 000 000
-
-
---------------- Examples -----------------
-
-
-Input 1:                Output 1:
--------                 --------
-25                      positive
-
-
-------------------------------------------
-
-
-Input 2:                Output 2:
--------                 --------
-0.7                     small positive
-
-
-------------------------------------------
-
-
-Input 3:                Output 3:
--------                 --------
-435247392.921           large positive
-
-
-------------------------------------------
-
-
-Input 4:                Output 4:
--------                 --------
--0.005                  small negative
-
-
-------------------------------------------
-
-
-Input 5:                Output 5:
--------                 --------
--103.21                 negative
-
-
-------------------------------------------
-
-
-Input 6:                Output 6:
--------                 --------
--358583355123.001       large negative
-
-
-"""
