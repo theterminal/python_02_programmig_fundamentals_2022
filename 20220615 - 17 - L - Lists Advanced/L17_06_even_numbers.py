f9ef1ce768b493ab259eb1cbb039fcf2d99256d2
theterminal <kirilk777@gmail.com>
2022-12-09 08:20:25 +0200

    Refactored

--- 20220615 - 17 - L - Lists Advanced/L17_06_even_numbers.py
+++ 20220615 - 17 - L - Lists Advanced/L17_06_even_numbers.py
@@ -9,39 +9,3 @@ numbers = list(map(int, input().split(', ')))
 indices = [i for i in range(len(numbers)) if numbers[i] % 2 == 0]
 
 print(indices)
-
-
-""" _____________ Even Numbers ______________
-
-
-Write a program that reads a single string with numbers separated by comma and space ", ". 
-Print the indices of all even numbers.
-
-__________ Test Data ____________
-
-
-Input 1:
--------
-3, 2, 1, 5, 8
-
-
-Output 1:
---------
-[1, 4]
-
-
----------------------------------
-
-
-Input 2:
--------
-2, 4, 6, 9, 10
-
-
-Output 2:
---------
-[0, 1, 2, 4]
-
-
---------------------------------
-"""
