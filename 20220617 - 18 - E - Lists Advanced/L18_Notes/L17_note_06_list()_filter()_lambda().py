113508c03ab6a5d9dddc9eb2dd716e56bdc329a6
theterminal <kirilk777@gmail.com>
2022-12-14 09:22:01 +0200

    Refactored

--- 20220617 - 18 - E - Lists Advanced/L18_Notes/L17_note_06_list()_filter()_lambda().py
+++ 20220617 - 18 - E - Lists Advanced/L18_Notes/L17_note_06_list()_filter()_lambda().py
@@ -1,33 +1,31 @@
 # 20220617 - Python - Lists Advance - Lecture
-# Notes - 06 - filter()
+# Note - 06 - filter()
 
 
 print('\n\n------ ex. 1 ----- list(filter(lambda x: condition, list_name)----------\n\n')
 
 
+# Returns a list of even numbers
 numbers_list = [1, 2, 3, 4, 5, 6]
 even_numbers = list(filter(lambda x: x % 2 == 0, numbers_list))
 
-print(even_numbers)
+print(even_numbers)                                                       # [2, 4, 6]
 
 
 print('\n\n------ ex. 2 ----- list(map(action, list_name); map(lambda x:); list(filter(lambda x: condition, list_name)----------\n\n')
 
 
-# Convert the list of strings into a list of numbers
-number_list = list(map(int, input().split(", ")))           # enter 1, 2, 3, 4, 5, 6, 7, 8, 9
+# Converting the list of strings into a list of numbers
+number_list = list(map(int, input().split(", ")))                         # enter 1, 2, 3, 4, 5, 6, 7, 8, 9
 print(number_list)
 
 # Find all the even numbers' indices
 found_indices_or_no = map(lambda x: x if number_list[x] % 2 == 0 else 'no', range(len(number_list)))
-print(found_indices_or_no)
+print(found_indices_or_no)                                                # Prints the address of the map object: <map object at 0x10bd77250>
 
 found_indices_or_no = list(found_indices_or_no)
-print(found_indices_or_no)
+print(found_indices_or_no)                                                # ['no', 1, 'no', 3, 'no', 5, 'no', 7, 'no']
 
 # Filter only the indices
 even_indices = list(filter(lambda a: a != 'no', found_indices_or_no))
-print(even_indices)
-
-
-print('-----------------------------------')
+print(even_indices)                                                       # [1, 3, 5, 7]
