67dcd246a2d694e3421ce0662871aa3b7fededbe
theterminal <kirilk777@gmail.com>
2022-12-14 12:41:14 +0200

    Refactored

--- 20220617 - 18 - E - Lists Advanced/L18_Notes/L18_note_01_matrix.py
+++ 20220617 - 18 - E - Lists Advanced/L18_Notes/L18_note_01_matrix.py
@@ -1,10 +1,12 @@
 # 20220620 - Python - List Advanced - Exercise
-# Notes 01
+# L18 - Note 01
 
 
-# prints table 25 * 25
-working_range = range(int(input('\nEnter range start number: ')), int(input('Enter range end number: ')) + 1, int(input('Enter stet: ')))
+print('\nMatrix with start value of 1, user specified end range, step value of 1, for column and row 1')
+print('The rest of the matrix is the multiplication table of the column 1 with row 1\n')
 
+
+working_range = range(1, int(input('Enter range end number: ')) + 1)
 lst_matrix = [[i for i in working_range], [i for i in working_range]]
 print()
 
@@ -20,7 +22,9 @@ for i in lst_matrix[0]:
 print()
 
 
-# prints table 10 * 10
+print('_____ example_2 _____prints table 10 * 10 _____\n')
+
+
 lst_matrix = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]
 
 for i in lst_matrix[0]:
@@ -33,7 +37,9 @@ for i in lst_matrix[0]:
 print()
 
 
-# prints table 9 * 9
+print('_____ example_3 _____prints table 9 * 9 _____\n')
+
+
 lst_matrix = [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]]
 
 for i in lst_matrix[0]:
