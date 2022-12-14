b5e20c2a0dd57703fa6d26f54c4f955d15a3297a
theterminal <kirilk777@gmail.com>
2022-12-14 09:13:10 +0200

    Refactored

--- 20220617 - 18 - E - Lists Advanced/L18_Notes/L17_note_05_list()_map()_lambda().py
+++ 20220617 - 18 - E - Lists Advanced/L18_Notes/L17_note_05_list()_map()_lambda().py
@@ -1,16 +1,16 @@
 # 20220617 - Python - Lists Advance - Lecture
-# Notes - 05 - map()
+# Note - 05 - list(), map(), lambda()
 
 
 powers_2 = list(map(lambda x: x * 2, [1, 2, 3, 4, 5]))
-print(powers_2)
+print(powers_2)                                                         # [2, 4, 6, 8, 10]
 
 
 # -----------------------------------------------------
 
 
 powers_2 = list(map(lambda x: x * 2, list(range(5))))
-print(powers_2)
+print(powers_2)                                                         # [0, 2, 4, 6, 8]
 
 
 # -----------------------------------------------------
@@ -20,4 +20,4 @@ def power_n(x):
     return x ** 2
 
 
-print(list(map(power_n, [1, 2, 3, 4, 5])))
+print(list(map(power_n, [1, 2, 3, 4, 5])))                              # [1, 4, 9, 16, 25]
