af4f17cf73c082c9ee110a37bf9ac3225b07d131
theterminal <kirilk777@gmail.com>
2022-12-16 09:10:19 +0200

    Refactored

--- 20220706 - 24 - L - Dictionaries/L24_Notes_and_Dictionaries_methods/L24_note_05_printing_adding_data.py
+++ 20220706 - 24 - L - Dictionaries/L24_Notes_and_Dictionaries_methods/L24_note_05_printing_adding_data.py
@@ -1,16 +1,16 @@
 # 20220712 - Python - Dictionaries - Lecture
-# Notes - 04 - Dictionaries
+# Note - 05 - Dictionaries, printing, adding data
 
 
 dict_level_1 = {
-    'Jerry': '941-416-2644',                # creating a dictionary with some data
+    'Jerry': '941-416-2644',                            # creating a dictionary with some data
     'Jim': '941-233-6564',
     'Jenn': '941-228-1312'
 }
 
 print(dict_level_1)                                     # printing the content of the dictionary
 
-dict_level_1['Kiril'] = '941-264-4222'                   # adding a new 'key:value' pair to the dictionary
+dict_level_1['Kiril'] = '941-264-4222'                  # adding a new 'key:value' pair to the dictionary
 print(dict_level_1)
 
 print(list(dict_level_1))                               # prints out a list of all 'keys' in the dictionary
