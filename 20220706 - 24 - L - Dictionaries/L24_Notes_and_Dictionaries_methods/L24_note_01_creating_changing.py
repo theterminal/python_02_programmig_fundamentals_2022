c9c40813f32cfb2e5b4d560888c1444671548484
theterminal <kirilk777@gmail.com>
2022-12-16 09:09:39 +0200

    Refactored

--- 20220706 - 24 - L - Dictionaries/L24_Notes_and_Dictionaries_methods/L24_note_01_creating_changing.py
+++ 20220706 - 24 - L - Dictionaries/L24_Notes_and_Dictionaries_methods/L24_note_01_creating_changing.py
@@ -1,5 +1,5 @@
 # 20220706 - Python - Dictionaries - Lecture
-# Notes - 01
+# Note - 01 - Creating dictionary, changing keys and values
 
 
 print("\n-------------- creating a dictionary ------------\n")
@@ -63,24 +63,3 @@ print("\n---------------------------------------------------\n")
 
 for j, k in my_dict.items():
     print(j, k)
-
-
-print("\n------------ combining dictionaries ---------------\n")
-
-
-a1 = {'el_1': 1, 'el_2': 2}
-a2 = {'el_3': 3, 'el_4': 4}
-a = {**a1, **a2}
-print(a)
-
-
-print("\n------------ converting lists into dictionaries ---\n")
-
-
-lst_1 = ['a', 'b', 'c', 'd']
-lst_2 = [1, 2, 3, 4]
-print(f'{lst_1}\n{lst_2}\n')
-
-dct_1 = dict(zip(lst_1, lst_2))
-
-print(dct_1)
