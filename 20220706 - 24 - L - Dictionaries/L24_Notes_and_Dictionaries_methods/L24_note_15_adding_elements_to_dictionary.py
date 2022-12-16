83eb1d74cdd812b26044cafe2e8537d85df1af1c
theterminal <kirilk777@gmail.com>
2022-12-16 09:11:32 +0200

    Refactored

---
+++ 20220706 - 24 - L - Dictionaries/L24_Notes_and_Dictionaries_methods/L24_note_15_adding_elements_to_dictionary.py
@@ -0,0 +1,14 @@
+# 20220712 - Python - Dictionaries - Lecture
+# Note 15 - Adding elements to a dictionary
+
+
+dict_1 = {
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen'
+}
+print(dict_1)                   # {11: 'eleven', 12: 'twelve', 13: 'thirteen'}
+print(dict_1[11])               # eleven
+
+dict_1[14] = 'fourteen'
+print(dict_1)                   # {11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen'}
