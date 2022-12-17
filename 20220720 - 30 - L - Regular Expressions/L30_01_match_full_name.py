64ad6513d6977b053724ee2acff9c3ad5cc6b9c8
theterminal <kirilk777@gmail.com>
2022-12-17 20:29:02 +0200

    Refactored

--- 20220720 - 30 - L - Regular Expressions/L30_01_match_full_name.py
+++ 20220720 - 30 - L - Regular Expressions/L30_01_match_full_name.py
@@ -1,8 +1,9 @@
 # 20220722 - Python Code - Regular Expressions - Lecture
 # 01 - Match Full Name - judge url: https://judge.softuni.org/Contests/Practice/Index/1742#0
 
+# use the link to construct the regex: https://regex101.com/
+
 import re
-# use this to construct the regex: https://regex101.com/
 
 
 # ------------------------ version 1 -------------------- judge: 100%
